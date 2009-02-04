d6c1d19cfcfdabf27a96bb7ce7986c5546e09fca
Joshua Timberman <jtimberman@cider.local>
2009-02-04 10:33:11 -0700

    Add first stab at djbdns cookbook

---
+++ djbdns/attributes/djbdns.rb
@@ -0,0 +1,25 @@
+#
+# Cookbook Name:: djbdns
+# Recipe:: default
+# Author:: Joshua Timberman (<joshua@opscode.com>)
+#
+# Copyright 2009, Opscode, Inc
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+djbdns Mash.new unless attribute?("djbdns")
+
+djbdns[:tinydns_ipaddress] = "127.0.0.1" unless djbdns.has_key?(:tinydns_ipaddress)
+djbdns[:tinydns_internal_ipaddress] = "127.0.0.1" unless djbdns.has_key?(:tinydns_internal_ipaddress)
+djbdns[:axfrdns_ipaddress] = "127.0.0.1" unless djbdns.has_key?(:axfrdns_ipaddress)
+djbdns[:public_dnscache_ipaddress] = node[:ipaddress] unless djbdns.has_key?(:public_dnscache_ipaddress)
---
+++ djbdns/recipes/cache.rb
@@ -0,0 +1,49 @@
+#
+# Cookbook Name:: djbdns
+# Recipe:: default
+# Author:: Joshua Timberman (<joshua@opscode.com>)
+#
+# Copyright 2009, Opscode, Inc
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+include_recipe "djbdns"
+
+execute "/usr/local/bin/dnscache-conf dnscache dnslog /etc/public-dnscache #{node[:djbdns][:public_dnscache_ipaddress]}" do
+  only_if "/usr/bin/test ! -d /etc/public-dnscache"
+  notifies :action, resources("execute[public_cache_update]")
+end
+
+execute "public_cache_update" do
+  cwd "/etc/public-dnscache"
+  command "/usr/local/bin/dnsip `/usr/local/bin/dnsqr ns . | awk '/answer:/ { print \$5 ; }' | sort` > root/servers/@"
+  notify_only true
+end
+
+template "/etc/public-dnscache/run" do
+  source "sv-cache-run.erb"
+  mode 0755
+end
+
+template "/etc/public-dnscache/log/run" do
+  source "sv-cache-log-run.erb"
+  mode 0755
+end
+
+link "#{node[:runit_service_dir]}/public-dnscache" do
+  to "/etc/public-dnscache"
+end
+
+link "/etc/init.d/public-dnscache" do
+  to node[:runit_sv_bin]
+end
