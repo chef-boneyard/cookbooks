84180e24c8a33525a22ea13e8369c2ff5f9c866f
Joshua Timberman <jtimberman@cider.local>
2009-03-10 13:58:37 -0600

    Add perl cookbook, with cpan_module as a define (should move to provider, chef-170)

---
+++ perl/files/default/Config.pm
@@ -0,0 +1,43 @@
+# This is CPAN.pm's systemwide configuration file. This file provides
+# defaults for users, and the values can be changed in a per-user
+# configuration file. The user-config file is being looked for as
+# ~/.cpan/CPAN/MyConfig.pm.
+
+$CPAN::Config = {
+  'build_cache' => q[10],
+  'build_dir' => q[/root/.cpan/build],
+  'cache_metadata' => q[1],
+  'cpan_home' => q[/root/.cpan],
+  'cpan_version_check' => q[1],
+  'dontload_hash' => {  },
+  'ftp' => q[/usr/bin/ftp],
+  'ftp_proxy' => q[],
+  'getcwd' => q[cwd],
+  'gpg' => q[/usr/bin/gpg],
+  'gzip' => q[/bin/gzip],
+  'histfile' => q[/root/.cpan/histfile],
+  'histsize' => q[100],
+  'http_proxy' => q[],
+  'inactivity_timeout' => q[0],
+  'index_expire' => q[1],
+  'inhibit_startup_message' => q[0],
+  'keep_source_where' => q[/root/.cpan/sources],
+  'lynx' => q[],
+  'make' => q[/usr/bin/make],
+  'make_arg' => q[],
+  'make_install_arg' => q[],
+  'makepl_arg' => q[INSTALLDIRS=site],
+  'ncftp' => q[],
+  'ncftpget' => q[],
+  'no_proxy' => q[],
+  'pager' => q[/usr/bin/less],
+  'prerequisites_policy' => q[ask],
+  'scan_cache' => q[atstart],
+  'shell' => q[/bin/bash],
+  'tar' => q[/bin/tar],
+  'term_is_latin' => q[1],
+  'unzip' => q[/usr/bin/unzip],
+  'urllist' => [q[http://ftp.osuosl.org/pub/CPAN/]],
+  'wget' => q[/usr/bin/wget],
+};
+1;
