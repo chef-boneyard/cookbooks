72f4e0c0c969b3515932dd24e326f07a439ffddf
jtimberman <joshua@opscode.com>
2009-03-13 17:04:06 -0600

    cook-54, initial commit of openldap
    Get auth and client classes into repo

---
+++ openldap/attributes/openldap.rb
@@ -0,0 +1,57 @@
+# Cookbook Name:: openldap
+# Attributes:: openldap
+#
+# Copyright 2008, Opscode, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+openldap Mash.new unless attribute?("openldap")
+
+openldap[:basedn] = nil unless openldap.has_key?(:basedn)
+openldap[:server] = "ldap.#{domain}" unless openldap.has_key?(:server)
+
+# File and directory locations for openldap.
+case platform
+when "redhat","centos"
+  openldap[:dir] = "/etc/openldap"
+  openldap[:run_dir] = "/var/run/openldap"
+  openldap[:module_dir] = "/usr/lib64/openldap"  
+when "debian","ubuntu"
+  openldap[:dir] = "/etc/ldap"
+  openldap[:run_dir] = "/var/run/slapd"
+  openldap[:module_dir] = "/usr/lib/ldap"
+else
+  openldap[:dir] = "/etc/ldap"
+  openldap[:run_dir] = "/var/run/slapd"
+  openldap[:module_dir] = "/usr/lib/ldap"
+end
+
+openldap[:ssl_dir] = "#{openldap[:dir]}/ssl"
+openldap[:cafile] = "#{openldap[:ssl_dir]}/ca.crt"
+
+# Auth & Client settings.
+openldap[:auth_type] = "openldap"
+openldap[:auth_binddn] = nil unless openldap.has_key?(:auth_binddn)
+openldap[:auth_bindpw] = nil unless openldap.has_key?(:auth_bindpw)
+openldap[:auth_url] = nil unless openldap.has_key?(:auth_url)
+
+# Server settings.
+openldap[:slapd_type] = nil unless openldap.has_key?(:slapd_type)
+
+if openldap[:slapd_type] == "slave"
+  master = search(:nodes, 'openldap_slapd_type:master') 
+  openldap[:slapd_master] = master unless openldap.has_key?(:slapd_master)
+  openldap[:slapd_replpw] = nil unless openldap.has_key?(:slapd_replpw)
+  openldap[:slapd_rid] = 102 unless openldap.has_key?(:slapd_rid)
+end
---
+++ openldap/recipes/auth.rb
@@ -0,0 +1,64 @@
+#
+# Cookbook Name:: openldap
+# Recipe:: auth
+#
+# Copyright 2008, Opscode, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+include_recipe "openldap::client"
+include_recipe "openssh"
+include_recipe "nscd"
+
+package "libnss-ldap" do
+  action :upgrade
+end
+
+package "libpam-ldap" do
+  action :upgrade
+end
+
+%w{ ldap.conf libnss-ldap.conf }.each do |cfg|
+  template "/etc/#{cfg}" do
+    source "#{cfg}.erb"
+    mode 0644
+    owner "root"
+    group "root"
+  end    
+end
+
+remote_file "/etc/nsswitch.conf" do
+  source "nsswitch.conf"
+  mode 0644
+  owner "root"
+  group "root"
+  notifies :restart, resources(:service => "nscd")
+  notifies :run, resources(:execute => "nscd-clear-passwd", :execute => "nscd-clear-group")
+end
+
+%w{ account auth password session }.each do |pam|
+  remote_file "/etc/pam.d/common-#{pam}" do
+    mode 0644
+    owner "root"
+    group "root"
+    notifies :restart, resources(:service => "ssh"), :delayed
+  end
+end
+
+template "/etc/security/login_access.conf" do
+  source "login_access.conf.erb"
+  mode 0644
+  owner "root"
+  group "root"
+end
