cf0ed71bc685a721c4bc194f3679eef418f654ec
jtimberman <joshua@opscode.com>
2009-03-18 16:48:27 -0600

    cook-8 incorrect mode with 644, using 0644

--- djbdns/recipes/server.rb
+++ djbdns/recipes/server.rb
@@ -31,7 +31,7 @@ end
 
 template "#{node[:runit_sv_dir]}/tinydns/root/data" do
   source "tinydns-data.erb"
-  mode 644
+  mode 0644
   notifies :run, resources("execute[build-tinydns-data]")
 end
 
