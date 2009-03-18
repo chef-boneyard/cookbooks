cef785adf1332081dbc6e61fe8ab2de55d376157
jtimberman <joshua@opscode.com>
2009-03-18 16:37:13 -0600

    cook-8, switch tinydns, tinydns internal servers to runit

--- djbdns/recipes/server.rb
+++ djbdns/recipes/server.rb
@@ -19,36 +19,20 @@
 #
 include_recipe "djbdns"
 
-execute "#{node[:djbdns][:bin_dir]}/tinydns-conf tinydns dnslog /etc/tinydns #{node[:djbdns][:tinydns_ipaddress]}" do
-  only_if "/usr/bin/test ! -d /etc/tinydns"
+execute "#{node[:djbdns][:bin_dir]}/tinydns-conf tinydns dnslog #{node[:runit_sv_dir]}/tinydns #{node[:djbdns][:tinydns_ipaddress]}" do
+  only_if "/usr/bin/test ! -d #{node[:runit_sv_dir]}/tinydns"
 end
 
 execute "build-tinydns-data" do
-  cwd "/etc/tinydns/root"
+  cwd "#{node[:runit_sv_dir]}/tinydns/root"
   command "make"
   action :nothing
 end
 
-template "/etc/tinydns/root/data" do
+template "#{node[:runit_sv_dir]}/tinydns/root/data" do
   source "tinydns-data.erb"
   mode 644
   notifies :run, resources("execute[build-tinydns-data]")
 end
 
-template "/etc/tinydns/run" do
-  source "sv-server-run.erb"
-  mode 0755
-end
-
-template "/etc/tinydns/log/run" do
-  source "sv-server-log-run.erb"
-  mode 0755
-end
-
-link "#{node[:runit_service_dir]}/tinydns" do
-  to "/etc/tinydns"
-end
-
-link "/etc/init.d/tinydns" do
-  to node[:runit_sv_bin]
-end
+runit_service "tinydns"
