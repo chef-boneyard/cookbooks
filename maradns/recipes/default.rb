830b064d6aa89eba52a7e262cb5f10eb3d563701
jtimberman <joshua@opscode.com>
2009-03-19 10:52:40 -0600

    cook-19, template source should be .erb

--- maradns/recipes/default.rb
+++ maradns/recipes/default.rb
@@ -44,7 +44,7 @@ end
 
 # be sure to create the db.domain template.
 template "/etc/maradns/db.#{node[:domain]}" do
-  source "db.#{node[:domain]}"
+  source "db.#{node[:domain]}.erb"
   mode 0644
   owner "root"
   group "root"
