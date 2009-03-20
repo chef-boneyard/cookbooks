c8fc1c1811d720d2c99bcbacb49d0f542d951fc3
jtimberman <joshua@opscode.com>
2009-03-20 16:50:29 -0600

    cook-37, add passenger cookbook

---
+++ passenger/attributes/passenger.rb
@@ -0,0 +1,10 @@
+gems_path `gem env gemdir`.chomp!
+ruby_path `which ruby`.chomp!
+
+passenger Mash.new unless attribute?("passenger")
+
+passenger[:version]          = "2.1.2" unless passenger.has_key?(:version)
+passenger[:root_path]        = "#{gems_path}/gems/passenger-#{passenger[:version]}"
+passenger[:module_path]      = "#{passenger[:root_path]}/ext/apache2/mod_passenger.so"
+passenger[:apache_load_path] = "#{apache[:dir]}/mods-available/passenger.load"
+passenger[:apache_conf_path] = "#{apache[:dir]}/mods-available/passenger.conf"
---
+++ passenger/recipes/default.rb
@@ -0,0 +1,57 @@
+#
+# Cookbook Name:: passenger
+# Recipe:: default
+#
+# Author:: Joshua Timberman (<joshua@opscode.com>)
+# Author:: Joshua Sierles (<joshua@37signals.com>)
+# Author:: Michael Hale (<mikehale@gmail.com>)
+# 
+# Copyright:: 2009, Opscode, Inc
+# Copyright:: 2009, 37signals
+# Coprighty:: 2009, Michael Hale
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+include_recipe "apache2"
+include_recipe "ruby"
+
+%w{ apache2-prefork-dev libapr1-dev }.each do |pkg|
+  package pkg do
+    action :upgrade
+  end
+end
+
+gem_package "passenger" do
+  version node[:passenger_version]
+end
+
+execute "passenger_module" do
+  command 'echo -en "\n\n\n\n" | passenger-install-apache2-module'
+  creates node[:passenger][:module_path]
+end
+
+template node[:passenger][:apache_load_path] do
+  source "passenger.load.erb"
+  owner "root"
+  group "root"
+  mode 0755
+end
+
+template node[:passenger][:apache_conf_path] do
+  source "passenger.conf.erb"
+  owner "root"
+  group "root"
+  mode 0755
+end
+
+apache_module "passenger"
