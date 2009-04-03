486c14b460d5ad2ddeacb5807b2c5e4819e8a44d
jtimberman <joshua@opscode.com>
2009-04-03 14:16:11 -0600

    COOK-64, Baseline recipe per hadoop quickstart.

---
+++ hadoop/attributes/hadoop.rb
@@ -0,0 +1,31 @@
+#
+# Cookbook Name:: hadoop
+# Attributes:: hadoop
+#
+# Copyright 2009, Opscode, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+hadoop Mash.new unless attribute?("hadoop")
+hadoop[:mirror_url] = "http://apache.osuosl.org/hadoop/core" unless hadoop.has_key?(:mirror_url)
+hadoop[:version] = "0.19.1" unless hadoop.has_key?(:version)
+hadoop[:uid] = 991          unless hadoop.has_key?(:uid)
+hadoop[:gid] = 65534        unless hadoop.has_key?(:gid)
+
+case platform
+when "redhat","centos","fedora"
+  hadoop[:java_home] = "/usr/lib/jvm/jre-#{languages[:java][:version].gsub(/_0/, '-openjdk')}" unless hadoop.has_key?(:java_home)
+when "debian","ubuntu"
+  hadoop[:java_home] = "/usr/lib/jvm/java-6-sun-#{languages[:java][:version].gsub(/_/, '.')}" unless hadoop.has_key?(:java_home)
+end
--- hadoop/recipes/default.rb
+++ hadoop/recipes/default.rb
@@ -19,3 +19,45 @@
 
 include_recipe "java"
 
+user "hadoop" do
+  shell "/bin/false"
+  home "/home/hadoop"
+  uid node[:hadoop][:uid]
+  gid node[:hadoop][:gid]
+  supports :manage_home => false
+  action :create
+end
+
+remote_file "/tmp/hadoop-#{node[:hadoop][:version]}.tar.gz" do
+  source "#{node[:hadoop][:mirror_url]}/hadoop-#{node[:hadoop][:version]}/hadoop-#{node[:hadoop][:version]}.tar.gz"
+  not_if do FileTest.directory?("/srv/hadoop-#{node[:hadoop][:version]}") end
+end
+
+execute "tar -xzf /tmp/hadoop-#{node[:hadoop][:version]}.tar.gz" do
+  cwd "/srv"
+  creates "/srv/hadoop-#{node[:hadoop][:version]}"
+end
+
+link "/srv/hadoop" do
+  to "/srv/hadoop-#{node[:hadoop][:version]}"
+end
+
+%w{ /etc/hadoop /etc/hadoop/env /var/log/hadoop }.each do |dir|
+  directory dir 
+end
+
+%w{ hadoop-env.sh hadoop-site.xml }.each do |cfg|
+  template "/etc/hadoop/#{cfg}" do
+    source "#{cfg}.erb"
+    owner "root"
+    group "root"
+    mode 0644
+  end
+end
+  
+template "/usr/bin/hadoop" do
+  source "hadoop.erb"
+  owner "root"
+  group "root"
+  mode 0755
+end
