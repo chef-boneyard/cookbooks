1ee186e9306a4e1ff8f288474e72cf15ecb7927a
jtimberman <joshua@opscode.com>
2009-04-02 20:11:57 -0600

    update default version

--- passenger/attributes/passenger.rb
+++ passenger/attributes/passenger.rb
@@ -3,7 +3,7 @@ ruby_path `which ruby`.chomp!
 
 passenger Mash.new unless attribute?("passenger")
 
-passenger[:version]          = "2.1.2" unless passenger.has_key?(:version)
+passenger[:version]          = "2.1.3" unless passenger.has_key?(:version)
 passenger[:root_path]        = "#{gems_path}/gems/passenger-#{passenger[:version]}"
 passenger[:module_path]      = "#{passenger[:root_path]}/ext/apache2/mod_passenger.so"
 passenger[:apache_load_path] = "#{apache[:dir]}/mods-available/passenger.load"
