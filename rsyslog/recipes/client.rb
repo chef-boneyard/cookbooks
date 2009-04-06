28bfbaf3bd9eadd476b2694615d0a001335c71cf
jtimberman <joshua@opscode.com>
2009-04-06 17:01:50 -0600

    COOK-28, add rsyslog cookbook

---
+++ rsyslog/recipes/client.rb
@@ -0,0 +1,31 @@
+#
+# Cookbook Name:: rsyslog
+# Recipe:: client
+#
+# Copyright 2009, Opscode, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+include_recipe "rsyslog"
+
+rsyslog_server = node[:rsyslog][:server] ? node[:rsyslog][:server] : search(:node, "recipe:rsyslog::server").first["fqdn"]
+
+template "/etc/rsyslog.d/remote.conf" do
+  source "remote.conf.erb"
+  variables :server => rsyslog_server
+  owner "root"
+  group "root"
+  mode 0644
+  notifies :restart, resources(:service => "rsyslog"), :delayed
+end
