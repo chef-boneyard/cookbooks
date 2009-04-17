ac73579a5226c925b80b79e5ef99b93b58fb261b
jtimberman <joshua@opscode.com>
2009-04-17 12:37:48 -0600

    COOK-80, initial commit, chef-server is a passenger app!
    Deploy chef-server as a passenger app using some config from
    37signals cookbooks and the merb + passenger documentation.
    
    Attribute cleanup and add a chef client recipe. Adjust /etc/chef
    config files for some 'sane' defaults for deployment.
    
    Remove iptables remnants.

--- chef/attributes/client.rb
+++ chef/attributes/client.rb
@@ -1,9 +1,11 @@
 #
 # Author:: Joshua Timberman <joshua@opscode.com>
+# Author:: Joshua Sierles <joshua@37signals.com>
 # Cookbook Name:: chef
-# Attributes:: server
+# Attributes:: client
 #
 # Copyright 2008-2009, Opscode, Inc
+# Copyright 2009, 37signals
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -18,5 +20,8 @@
 # limitations under the License.
 
 chef Mash.new unless attribute?("chef")
-
-chef[:path] = "/srv/chef" unless chef.has_key?(:path)
+# chef[:client_path] = `which chef-client`.chomp
+chef[:client_version] = "0.5.7" unless chef.has_key?(:client_version)
+chef[:client_interval] = "1800" unless chef.has_key?(:client_interval)
+chef[:client_splay] = "20"      unless chef.has_key?(:client_splay)
+chef[:client_log] = "/var/log/chef/client.log" unless chef.has_key?(:client_log)
--- chef/recipes/server.rb
+++ chef/recipes/server.rb
@@ -1,9 +1,11 @@
 #
 # Author:: Joshua Timberman <joshua@opscode.com>
+# Author:: Joshua Sierles <joshua@37signals.com>
 # Cookbook Name:: chef
 # Recipe:: server
 #
-# Copyright 2008, OpsCode, Inc
+# Copyright 2008-2009, Opscode, Inc
+# Copyright 2009, 37signals
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -20,25 +22,78 @@
 include_recipe "runit"
 include_recipe "couchdb"
 include_recipe "stompserver" 
+include_recipe "apache2"
+include_recipe "apache2::mod_ssl"
+include_recipe "passenger"
+include_recipe "chef::client"
+
+gem_package "chef-server" do
+  version node[:chef][:server_version]
+end
+
+if node[:chef][:server_version] >= "0.5.7"
+  gem_package "chef-server-slice" do
+    version node[:chef][:server_version]
+  end
+end
 
 directory "/etc/chef" do
-  owner "root"
+  owner "chef"
   mode 0755
 end
 
 template "/etc/chef/server.rb" do
-  owner "root"
+  owner "chef"
   mode 0644
   source "server.rb.erb"
   action :create
 end
 
-template "/etc/chef/client.rb" do
-  owner "root"
-  mode 0644
-  source "client.rb.erb"
-  action :create
+directory "/var/log/chef" do
+  owner "chef"
+  group "chef"
+  mode "775"
+end
+
+%w{ openid cache search_index openid/cstore certificates }.each do |dir|
+  directory "#{node[:chef][:path]}/#{dir}" do
+    owner "chef"
+    group "chef"
+    mode "775"
+  end
 end
 
 runit_service "chef-indexer" 
-runit_service "chef-server"
+
+template "#{node[:chef][:server_path]}/config.ru" do
+  source "config.ru.erb"
+  owner "chef"
+  group "chef"
+  mode "644"
+  notifies :restart, resources(:service => "apache2")
+end
+
+template "#{node[:chef][:server_path]}/config/environments/production.rb" do
+  source "merb-production.rb.erb"
+  action :create
+  owner "root"
+  group "root"
+  mode "664"
+  notifies :restart, resources(:service => "apache2")
+end
+
+template "#{node[:chef][:server_path]}/config/init.rb" do
+  source "chef-server.init.rb.erb"
+  action :create
+  owner "root"
+  group "root"
+  mode "664"
+  notifies :restart, resources(:service => "apache2")
+end
+
+web_app "chef_server" do
+  docroot "#{node[:chef][:server_path]}/public"
+  template "chef_server.conf.erb"
+  server_name "chef.#{node[:domain]}"
+  server_aliases "chef"
+end
