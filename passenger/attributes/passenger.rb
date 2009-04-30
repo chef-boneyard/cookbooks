b68f07840607efdfaa7e1a0c218eef5365af96ff
jtimberman <joshua@opscode.com>
2009-04-30 10:13:55 -0600

    cook-90, bump passenger version

--- passenger/attributes/passenger.rb
+++ passenger/attributes/passenger.rb
@@ -3,6 +3,6 @@ ruby_path `which ruby`.chomp!
 
 passenger Mash.new unless attribute?("passenger")
 
-passenger[:version]          = "2.2.1" unless passenger.has_key?(:version)
+passenger[:version]          = "2.2.2" unless passenger.has_key?(:version)
 passenger[:root_path]        = "#{gems_path}/gems/passenger-#{passenger[:version]}"
 passenger[:module_path]      = "#{passenger[:root_path]}/ext/apache2/mod_passenger.so"
