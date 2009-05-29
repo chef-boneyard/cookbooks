ca122339c5f76b7d531c830fddb71d4b430092d6
jtimberman <joshua@opscode.com>
2009-05-28 19:32:26 -0600

    cook-124, randomly generate a validation token, clarify values in config file for locking down openid.

--- chef/attributes/server.rb
+++ chef/attributes/server.rb
@@ -22,6 +22,10 @@
 # change to languages[:ruby][:gems_dir] when ohai is updated
 gems_path %x{gem env gemdir}.chomp!
 
+validation_token = ""
+chars = ("a".."z").to_a + ("A".."Z").to_a + ("0".."9").to_a
+20.times { |i| validation_token << chars[rand(chars.size-1)] }
+
 chef Mash.new unless attribute?("chef")
 chef[:server_version] = "0.6.2"  unless chef.has_key?(:server_version)
 chef[:server_log] = "/var/log/chef/server.log" unless chef.has_key?(:server_log)
@@ -30,3 +34,4 @@ chef[:server_hostname] = hostname unless chef.has_key?(:server_hostname)
 chef[:server_fqdn] = "#{chef[:server_hostname]}.#{domain}" unless chef.has_key?(:server_fqdn)
 chef[:server_ssl_req] = "/C=US/ST=Several/L=Locality/O=Example/OU=Operations/" +
   "CN=#{chef[:server_fqdn]}/emailAddress=ops@#{domain}" unless chef.has_key?(:server_ssl_req)
+chef[:server_token] = validation_token unless chef.has_key?(:server_token)
