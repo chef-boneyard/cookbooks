6505ef203f33755c6d658c4c03e59d93bba19340
jtimberman <joshua@opscode.com>
2009-06-24 16:34:35 -0700

    cook-141, bump version. oh and line up server vars

--- chef/attributes/server.rb
+++ chef/attributes/server.rb
@@ -27,11 +27,11 @@ chars = ("a".."z").to_a + ("A".."Z").to_a + ("0".."9").to_a
 20.times { |i| validation_token << chars[rand(chars.size-1)] }
 
 chef Mash.new unless attribute?("chef")
-chef[:server_version] = "0.7.0"  unless chef.has_key?(:server_version)
-chef[:server_log] = "/var/log/chef/server.log" unless chef.has_key?(:server_log)
-chef[:server_path] = "#{gems_path}/gems/chef-server-#{chef[:server_version]}"
+chef[:server_version]  = "0.7.2"  unless chef.has_key?(:server_version)
+chef[:server_log]      = "/var/log/chef/server.log" unless chef.has_key?(:server_log)
+chef[:server_path]     = "#{gems_path}/gems/chef-server-#{chef[:server_version]}"
 chef[:server_hostname] = hostname unless chef.has_key?(:server_hostname)
-chef[:server_fqdn] = "#{chef[:server_hostname]}.#{domain}" unless chef.has_key?(:server_fqdn)
-chef[:server_ssl_req] = "/C=US/ST=Several/L=Locality/O=Example/OU=Operations/" +
+chef[:server_fqdn]     = "#{chef[:server_hostname]}.#{domain}" unless chef.has_key?(:server_fqdn)
+chef[:server_ssl_req]  = "/C=US/ST=Several/L=Locality/O=Example/OU=Operations/" +
   "CN=#{chef[:server_fqdn]}/emailAddress=ops@#{domain}" unless chef.has_key?(:server_ssl_req)
-chef[:server_token] = validation_token unless chef.has_key?(:server_token)
+chef[:server_token]    = validation_token unless chef.has_key?(:server_token)
