d93c3fcef5e4ee361c0083a0b03ce34fdae6de4b
jtimberman <joshua@opscode.com>
2009-06-29 16:57:54 -0600

    bump cookbook version too

--- ec2/metadata.rb
+++ ec2/metadata.rb
@@ -2,7 +2,7 @@ maintainer        "Opscode, Inc."
 maintainer_email  "cookbooks@opscode.com"
 license           "Apache 2.0"
 description       "Manage EC2 metadata as attributes"
-version           "0.7"
+version           "0.8"
 
 attribute "ec2_metadata",
   :display_name => "EC2 Metadata",
