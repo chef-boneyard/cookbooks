d93e3c33c2a19241107ec2783ae39d3e01e14fae
jtimberman <joshua@opscode.com>
2009-07-01 18:06:57 -0600

    cook-53, dynomite probbly wants thrift too

---
+++ thrift/metadata.rb
@@ -0,0 +1,12 @@
+maintainer       "Opscode, Inc."
+maintainer_email "cookbooks@opscode.com"
+license          "Apache 2.0"
+description      "Installs/Configures thrift"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
+version          "0.1"
+
+supports "ubuntu"
+
+%w{ build-essential boost java subversion }.each do |cb|
+  depends cb
+end
