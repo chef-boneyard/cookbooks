56ffa7ac71d6a84e5a291701bcddf0de3a467fef
jtimberman <joshua@opscode.com>
2009-07-13 17:14:05 -0600

    update copright to reflect Scott rather than Opscode for this recipe :-)

--- ssh_known_hosts/recipes/default.rb
+++ ssh_known_hosts/recipes/default.rb
@@ -2,7 +2,7 @@
 # Cookbook Name:: ssh_known_hosts
 # Recipe:: default
 #
-# Copyright 2008, OpsCode, Inc.
+# Copyright 2009, Scott M. Likens
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
