d4b7ac5d611512f92e9a19b56f6d8eeeac860df5
Scott M. Likens <scott@likens.us>
2009-07-12 18:42:55 -0700

    - Update License.
    Signed-off-by: Scott M. Likens <scott@likens.us>

--- ssh_known_hosts/recipes/default.rb
+++ ssh_known_hosts/recipes/default.rb
@@ -1,12 +1,25 @@
 #
-# Cookbook Name:: snmp
+# Cookbook Name:: apache2
 # Recipe:: default
 #
-# Copyright 2009, Adapp, Inc.
+# Copyright 2008, OpsCode, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 
 require 'chef/log'
 
-my_fqdn = []
+my_hostname = []
 my_host_rsa_public = []
 my_host_ip = []
 
@@ -20,9 +33,9 @@ search(:node, "*") do |server|
     Chef::Log.debug("IP Address #{y}")
     my_host_ip << y
     end
-  server['fqdn'].each do |z|
-    Chef::Log.debug("FQDN #{z}")
-    my_fqdn << z
+  server['hostname'].each do |z|
+    Chef::Log.debug("hostname #{z}")
+    my_hostname << z
     end
   end
 
@@ -33,7 +46,7 @@ template "/etc/ssh/known_hosts" do
   group "root"
   variables(
     :ip => my_host_ip,
-    :fqdn => my_fqdn,
+    :hostname => my_hostname,
     :host_rsa_public => my_host_rsa_public
   )
 end
