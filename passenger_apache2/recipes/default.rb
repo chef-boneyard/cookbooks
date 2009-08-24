2ff093dcfdfcd57eab19642832a86662ce365fda
jtimberman <joshua@opscode.com>
2009-08-24 01:52:07 -0600

    cook-177, add httpd-devel on centos/redhat so passenger will compile

--- passenger_apache2/recipes/default.rb
+++ passenger_apache2/recipes/default.rb
@@ -26,16 +26,19 @@ include_recipe "packages"
 include_recipe "ruby"
 include_recipe "apache2"
 
-if platform?("centos","redhat") and dist_only?
-  # just the gem, we'll install the apache module within apache2
-  package "rubygem-passenger"
-
-  return
-end
-
-%w{ apache2-prefork-dev libapr1-dev }.each do |pkg|
-  package pkg do
-    action :upgrade
+if platform?("centos","redhat")
+  if dist_only?
+    # just the gem, we'll install the apache module within apache2
+    package "rubygem-passenger"
+    return
+  else
+    package "httpd-devel"
+  end
+else
+  %w{ apache2-prefork-dev libapr1-dev }.each do |pkg|
+    package pkg do
+      action :upgrade
+    end
   end
 end
 
