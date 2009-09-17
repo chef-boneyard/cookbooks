85b1318eb071b005ece3a411a79b63610ff1e24a
Dan DeLeo <danielsdeleo@mac.com>
2009-09-16 21:12:13 -0600

    include mod_dav_svn and svn client recipes

--- subversion/recipes/server.rb
+++ subversion/recipes/server.rb
@@ -17,6 +17,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+include_recipe "apache2::mod_dav_svn"
+include_recipe "subversion::client"
 
 directory node[:subversion][:repo_dir] do
   recursive true
