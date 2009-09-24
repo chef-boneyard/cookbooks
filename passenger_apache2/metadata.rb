0805f31e4c3a4a5734a2af054c92ceebfee99e79
jtimberman <joshua@opscode.com>
2009-09-24 13:47:16 -0600

    bump passenger version to latest release and rerake metadata

--- passenger_apache2/metadata.rb
+++ passenger_apache2/metadata.rb
@@ -3,7 +3,7 @@ maintainer_email  "cookbooks@opscode.com"
 license           "Apache 2.0"
 description       "Installs passenger for Apache2"
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-version           "0.12"
+version           "0.14"
 
 %w{ packages ruby apache2 rails }.each do |cb|
   depends cb
@@ -16,7 +16,7 @@ end
 attribute "passenger/version",
   :display_name => "Passenger Version",
   :description => "Version of Passenger to install",
-  :default => "2.2.4"
+  :default => "2.2.5"
 
 attribute "passenger/root_path",
   :display_name => "Passenger Root Path",
@@ -27,4 +27,3 @@ attribute "passenger/module_path",
   :display_name => "Passenger Module Path",
   :description => "Location of the compiled Apache module",
   :default => "passenger_root_path/ext/apache2/mod_passenger.so"
-
