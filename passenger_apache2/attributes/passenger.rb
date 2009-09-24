073a762e3f3da2b659b144413be09cc7f53c8001
jtimberman <joshua@opscode.com>
2009-09-24 13:49:23 -0600

    actually update the attribute file this time

--- passenger_apache2/attributes/passenger.rb
+++ passenger_apache2/attributes/passenger.rb
@@ -1,3 +1,3 @@
-default[:passenger][:version] = "2.2.4"
+default[:passenger][:version] = "2.2.5"
 set[:passenger][:root_path]   = "#{languages[:ruby][:gems_dir]}/gems/passenger-#{passenger[:version]}"
 set[:passenger][:module_path] = "#{passenger[:root_path]}/ext/apache2/mod_passenger.so"
