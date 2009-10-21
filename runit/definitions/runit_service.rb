6c0eb93cc58c3cb9e262d0448432f2baeee921b4
jtimberman <joshua@opscode.com>
2009-10-21 16:04:50 -0600

    allow cookbook to be specified for runit services

--- runit/definitions/runit_service.rb
+++ runit/definitions/runit_service.rb
@@ -18,58 +18,60 @@
 #
 
 define :runit_service, :directory => nil, :only_if => false, :options => Hash.new do
-  
+
   params[:directory] ||= node[:runit][:sv_dir]
-  
+
   sv_dir_name = "#{params[:directory]}/#{params[:name]}"
-  
+
   directory sv_dir_name do
     mode 0755
     action :create
   end
-  
+
   directory "#{sv_dir_name}/log" do
     mode 0755
     action :create
   end
-  
+
   directory "#{sv_dir_name}/log/main" do
     mode 0755
     action :create
   end
-  
+
   template "#{sv_dir_name}/run" do
     mode 0755
     source "sv-#{params[:name]}-run.erb"
+    cookbook params[:cookbook] if params[:cookbook]
     if params[:options].respond_to?(:has_key?)
       variables :options => params[:options]
     end
   end
-  
+
   template "#{sv_dir_name}/log/run" do
     mode 0755
     source "sv-#{params[:name]}-log-run.erb"
+    cookbook params[:cookbook] if params[:cookbook]
     if params[:options].respond_to?(:has_key?)
       variables :options => params[:options]
     end
   end
-  
+
   link "/etc/init.d/#{params[:name]}" do
     to node[:runit][:sv_bin]
   end
-  
+
   link "#{node[:runit][:service_dir]}/#{params[:name]}" do 
     to "#{sv_dir_name}"
   end
-  
+
   service params[:name] do
     supports :restart => true, :status => true
     action :nothing
   end
-  
+
   #execute "#{params[:name]}-down" do
   #  command "/etc/init.d/#{params[:name]} down"
   #  only_if do params[:only_if] end
   #end
-  
+
 end
