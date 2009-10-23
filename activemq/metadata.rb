1c6f278dac294865cb2cc33a0e32834983704bbb
jtimberman <joshua@opscode.com>
2009-10-23 15:26:42 -0600

    %w, not %x, in metadata.

--- activemq/metadata.rb
+++ activemq/metadata.rb
@@ -5,6 +5,6 @@ description      "Installs activemq and sets it up as a runit service"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
 version          "0.1"
 
-%x{java runit}.each do |cb|
+%w{java runit}.each do |cb|
   depends cb
 end
