59be5a43075c71f93c7f8bc47392cf2a70d2d557
chris <chris@opscodes-macbook-pro-2.local>
2009-11-04 21:45:16 +0000

    Initial commit of AWS cookbook with LWRPs for EBS volumes and elastic IPs

---
+++ aws/resources/ebs_volume.rb
@@ -0,0 +1,10 @@
+actions :create, :attach, :detach, :snapshot
+
+attribute :aws_access_key,        :kind_of => String
+attribute :aws_secret_access_key, :kind_of => String
+attribute :size,                  :kind_of => Integer
+attribute :snapshot_id,           :kind_of => String
+attribute :availability_zone,     :kind_of => String
+attribute :device,                :kind_of => String
+attribute :volume_id,             :kind_of => String
+attribute :timeout,               :default => 3*60 # 3 mins, nil or 0 for no timeout
