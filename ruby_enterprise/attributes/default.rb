5d5e5014b471a380846b4362718637965c0f3d8b
jtimberman <joshua@opscode.com>
2010-01-06 10:43:12 -0700

    COOK-107, ruby_enterprise, rails_enterprise, passenger_enterprise
    Adds the ruby_enterprise cookbook based on work by Sean Cribbs
    and Michael Hale (per ticket). Also adds passenger_enterprise and
    rails_enterprise cookboosk to handle using Ruby Enterprise Edition
    as the base Ruby for installing Rails.

--- ruby_enterprise/attributes/default.rb
+++
@@ -1,5 +0,0 @@
-ruby_enterprise_edition Mash.new unless attribute?("ruby_enterprise_edition")
-ruby_enterprise_edition[:version] = '20090520' unless ruby_enterprise_edition.has_key?(:version)
-ruby_enterprise_edition[:install_path] = '/opt/ruby-enterprise' unless ruby_enterprise_edition.has_key?(:install_path)
-ruby_enterprise_edition[:url] = "http://github.com/FooBarWidget/rubyenterpriseedition/tarball/release-#{ruby_enterprise_edition[:version]}"
-ruby_enterprise_edition[:cow_friendly] = %Q[#{ruby_enterprise_edition[:install_path]}/bin/ruby -e "exit 1 unless GC.respond_to?(:copy_on_write_friendly=)"]
