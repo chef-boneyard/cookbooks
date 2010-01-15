ca740a12d6affa170b170ee4595529852805563e
AJ Christensen <aj@junglist.gen.nz>
2010-01-16 07:09:10 +1300

    Move ec2 specific parts of MySQL cookbook
      Totally optional server_ec2 recipe

--- mysql/metadata.rb
+++ mysql/metadata.rb
@@ -3,9 +3,10 @@ maintainer_email  "cookbooks@opscode.com"
 license           "Apache 2.0"
 description       "Installs and configures mysql for client or server"
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-version           "0.14"
+version           "0.15"
 recipe            "mysql::client", "Installs packages required for mysql clients using run_action magic"
 recipe            "mysql::server", "Installs packages required for mysql servers w/o manual intervention"
+recipe            "mysql::server_ec2", "Performs EC2-specific mountpoint manipulation"
 
 %w{ debian ubuntu }.each do |os|
   supports os
