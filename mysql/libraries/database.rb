3e510e67776887e3dc75965771607d71387b70f4
AJ Christensen <aj@junglist.gen.nz>
2010-01-20 02:00:49 +1300

    Additional mysql library fixes

--- mysql/libraries/database.rb
+++ mysql/libraries/database.rb
@@ -1,8 +1,14 @@
+begin
+  require 'mysql'
+rescue LoadError
+  Chef::Log.warn("Missing gem 'mysql'")
+end
+
 module Opscode
   module Mysql
     module Database
       def db
-        @@db ||= Mysql.new new_resource.host, new_resource.username, new_resource.password
+        @@db ||= ::Mysql.new new_resource.host, new_resource.username, new_resource.password
       end
     end
   end
