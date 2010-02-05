1eb47eba45341de5a5b9c45d159ac1a44dd51cf9
jtimberman <joshua@opscode.com>
2010-02-05 14:41:11 -0700

    support additional platforms, add readme, fixes COOK-250, COOK-234

--- java/metadata.rb
+++ java/metadata.rb
@@ -2,7 +2,8 @@ maintainer        "Opscode, Inc."
 maintainer_email  "cookbooks@opscode.com"
 license           "Apache 2.0"
 description       "Installs java"
-version           "0.8"
+long_description  IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
+version           "0.9"
 
 %w{ debian ubuntu }.each do |os|
   supports os
