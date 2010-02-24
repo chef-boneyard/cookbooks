3da32522968d0a8933a5b311244b74c1d811db4f
jtimberman <joshua@opscode.com>
2010-02-24 15:59:47 -0700

    COOK-261, better search from 0.8 and additional plugins (nginx, memcached)

--- munin/metadata.rb
+++ munin/metadata.rb
@@ -2,8 +2,7 @@ maintainer        "Opscode, Inc."
 maintainer_email  "cookbooks@opscode.com"
 license           "Apache 2.0"
 description       "Instsalls and configures munin"
-version           "0.7"
+version           "0.8"
 
-%w{ debian ubuntu }.each do |os|
-  supports os
-end
+supports "debian"
+supports "ubuntu"
