f1b476f31fe1c618b55ece200dd10db703c3faa2
jtimberman <joshua@opscode.com>
2010-02-24 15:27:07 -0700

    COOK-252, remove vestiges of php

--- nagios/metadata.rb
+++ nagios/metadata.rb
@@ -2,8 +2,7 @@ maintainer        "Opscode, Inc."
 maintainer_email  "cookbooks@opscode.com"
 license           "Apache 2.0"
 description       "Installs and configures nagios"
-version           "0.2"
-depends           "php::php5"
+version           "0.3"
 
 %w{ debian ubuntu }.each do |os|
   supports os
