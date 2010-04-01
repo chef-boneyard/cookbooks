f290c9606eebd8065f497851f75fe894cffaa25a
b <b@b3k.us>
2010-04-01 12:30:02 -0700

    Skeleton for riak cookbook

---
+++ riak/metadata.rb
@@ -0,0 +1,6 @@
+maintainer        "Basho Technologies, Inc."
+maintainer_email  "riak@basho.com"
+license           "Apache 2.0"
+description       "Installs and configures Riak distributed data store"
+version           "0.1"
+
