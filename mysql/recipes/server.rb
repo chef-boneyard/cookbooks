66828f8b69318b64284f8474fb85b4d801760ae3
jtimberman <joshua@opscode.com>
2010-05-12 17:30:40 -0600

    update mysql cookbook to work on lucid

--- mysql/recipes/server.rb
+++ mysql/recipes/server.rb
@@ -45,7 +45,7 @@ when "debian","ubuntu"
     source "debian.cnf.erb"
     owner "root"
     group "root"
-    mode "0644"
+    mode "0600"
   end
 end
 
@@ -55,9 +55,13 @@ end
 
 service "mysql" do
   service_name value_for_platform([ "centos", "redhat", "suse" ] => {"default" => "mysqld"}, "default" => "mysql")
-
+  if (platform?("ubuntu") && node.platform_version.to_f >= 10.04)
+    restart_command "restart mysql"
+    stop_command "stop mysql"
+    start_command "start mysql"
+  end
   supports :status => true, :restart => true, :reload => true
-  action :enable
+  action :nothing
 end
 
 template value_for_platform([ "centos", "redhat", "suse" ] => {"default" => "/etc/my.cnf"}, "default" => "/etc/mysql/my.cnf") do
