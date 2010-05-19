8e64bfc5038b15ca025bd1fce2b999a5f26624e0
jtimberman <joshua@opscode.com>
2010-05-19 16:51:14 -0600

    fix typo in after fork

--- unicorn/metadata.rb
+++ unicorn/metadata.rb
@@ -3,6 +3,6 @@ maintainer_email "ops@opscode.com"
 license          "Apache 2.0"
 description      "Installs/Configures unicorn"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-version          "0.1"
+version          "0.1.1"
 depends          "ruby"
 depends          "rubygems"
