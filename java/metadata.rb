6226962418784acc88626ac0e68fda12019bc624
jtimberman <joshua@opscode.com>
2010-05-19 16:50:31 -0600

    readme update, notice of sun-java deprecation

--- java/metadata.rb
+++ java/metadata.rb
@@ -1,9 +1,9 @@
 maintainer        "Opscode, Inc."
 maintainer_email  "cookbooks@opscode.com"
 license           "Apache 2.0"
-description       "Installs java"
+description       "Installs java via openjdk."
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-version           "0.10.0"
+version           "0.10.1"
 
 %w{ debian ubuntu redhat centos fedora }.each do |os|
   supports os
