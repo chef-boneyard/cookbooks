733fbdaa5f9465c72f6669ad11554ff3603ffec3
jtimberman <joshua@opscode.com>
2010-05-19 16:49:56 -0600

    Create a cpan config for perl 5.10.1 (used in lucid)

--- perl/metadata.rb
+++ perl/metadata.rb
@@ -3,7 +3,7 @@ maintainer_email  "cookbooks@opscode.com"
 license           "Apache 2.0"
 description       "Installs perl and provides a define for maintaining CPAN modules"
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-version           "0.8"
+version           "0.9.0"
 
 %w{ ubuntu debian centos redhat }.each do |os|
   supports os
