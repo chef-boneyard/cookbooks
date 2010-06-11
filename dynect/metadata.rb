1c3450436c304d75e234d8638338c399ce48ca5b
jtimberman <joshua@opscode.com>
2010-06-11 12:18:37 -0600

    better description for dynect metadata

--- dynect/metadata.rb
+++ dynect/metadata.rb
@@ -1,6 +1,6 @@
 maintainer       "Opscode, Inc."
 maintainer_email "cookbooks@opscode.com"
 license          "Apache 2.0"
-description      "Installs/Configures dynect"
+description      "LWRP for managing DNS records with Dynect's REST API"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.2"
