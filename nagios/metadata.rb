d14f73354c77e3dbefa83fda975ff86040a312d8
jtimberman <joshua@opscode.com>
2010-06-19 12:27:38 -0600

    nagios server depends on apache2 cookbook

--- nagios/metadata.rb
+++ nagios/metadata.rb
@@ -2,8 +2,11 @@ maintainer        "Opscode, Inc."
 maintainer_email  "cookbooks@opscode.com"
 license           "Apache 2.0"
 description       "Installs and configures nagios"
-version           "0.3"
+long_description  IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
+version           "0.3.1"
 
 %w{ debian ubuntu }.each do |os|
   supports os
 end
+
+depends "apache2"
