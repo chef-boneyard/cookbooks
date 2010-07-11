f84813bf8258ae82fa29aa36dadb6ebedfa89db8
jtimberman <joshua@opscode.com>
2010-07-10 21:12:30 -0600

    COOK-324, update jira attributes

--- jira/metadata.rb
+++ jira/metadata.rb
@@ -2,7 +2,7 @@ maintainer        "Opscode, Inc."
 maintainer_email  "cookbooks@opscode.com"
 license           "Apache 2.0"
 description       "Installs and configures jira"
-version           "0.8.1"
+version           "0.8.2"
 recommends        "mysql"
 
 recipe "jira", "Installs and configures Jira"
