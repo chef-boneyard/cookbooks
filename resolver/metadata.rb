b47591e5810b30acfdb6c2ef132088f3641e64d5
jtimberman <joshua@opscode.com>
2010-07-10 21:54:57 -0600

    Add readme for 'resolver' cookbook

--- resolver/metadata.rb
+++ resolver/metadata.rb
@@ -2,7 +2,8 @@ maintainer        "Opscode, Inc."
 maintainer_email  "cookbooks@opscode.com"
 license           "Apache 2.0"
 description       "Configures /etc/resolv.conf"
-version           "0.8.1"
+long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version           "0.8.2"
 
 recipe "resolver", "Configures /etc/resolv.conf via attributes"
 
