7bf91c98c58d695aaebcc857f4d8c029cb5933aa
jtimberman <joshua@opscode.com>
2010-07-10 21:12:53 -0600

    COOK-324, update kickstart attributes

--- kickstart/attributes/default.rb
+++ kickstart/attributes/default.rb
@@ -1,2 +1,2 @@
-set_unless[:kickstart][:rootpw] = nil
-set_unless[:kickstart][:virtual_host_name] = "build.#{domain}"
+default[:kickstart][:rootpw] = nil
+default[:kickstart][:virtual_host_name] = "build.#{domain}"
