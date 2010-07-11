b8c7d19d69dff117ffd559fac6f3dec90df16dcc
jtimberman <joshua@opscode.com>
2010-07-10 21:55:03 -0600

    Add readme for 'ntp' cookbook

--- ntp/metadata.rb
+++ ntp/metadata.rb
@@ -2,7 +2,8 @@ maintainer        "Opscode, Inc."
 maintainer_email  "cookbooks@opscode.com"
 license           "Apache 2.0"
 description       "Installs and configures ntp as a client or server"
-version           "0.8.1"
+long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version           "0.8.2"
 
 recipe "ntp", "Installs and configures ntp either as a server or client"
 
