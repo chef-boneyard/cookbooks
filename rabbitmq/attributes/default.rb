5d8671a8fc7a3fd0884d6765692503ad4719ae28
jtimberman <joshua@opscode.com>
2010-07-10 21:19:26 -0600

    COOK-324, update rabbitmq attributes

--- rabbitmq/attributes/default.rb
+++ rabbitmq/attributes/default.rb
@@ -1,12 +1,12 @@
-set_unless[:rabbitmq][:nodename]  = "rabbit"
-set_unless[:rabbitmq][:address]  = "0.0.0.0"
-set_unless[:rabbitmq][:port]  = "5672"
-set_unless[:rabbitmq][:erl_args]  = "+K true +A 30 \
+default[:rabbitmq][:nodename]  = "rabbit"
+default[:rabbitmq][:address]  = "0.0.0.0"
+default[:rabbitmq][:port]  = "5672"
+default[:rabbitmq][:erl_args]  = "+K true +A 30 \
 -kernel inet_default_listen_options [{nodelay,true},{sndbuf,16384},{recbuf,4096}] \
 -kernel inet_default_connect_options [{nodelay,true}]"
-set_unless[:rabbitmq][:start_args] = ""
-set_unless[:rabbitmq][:logdir] = "/var/log/rabbitmq"
-set_unless[:rabbitmq][:mnesiadir] = "/var/lib/rabbitmq/mnesia"
-set_unless[:rabbitmq][:cluster] = "no"
-set_unless[:rabbitmq][:cluster_config] = "/etc/rabbitmq/rabbitmq_cluster.config"
-set_unless[:rabbitmq][:cluster_disk_nodes] = []
+default[:rabbitmq][:start_args] = ""
+default[:rabbitmq][:logdir] = "/var/log/rabbitmq"
+default[:rabbitmq][:mnesiadir] = "/var/lib/rabbitmq/mnesia"
+default[:rabbitmq][:cluster] = "no"
+default[:rabbitmq][:cluster_config] = "/etc/rabbitmq/rabbitmq_cluster.config"
+default[:rabbitmq][:cluster_disk_nodes] = []
