3642fe2d48512bfee195d06359e1a4b69849d8fa
jtimberman <joshua@opscode.com>
2010-07-10 21:21:58 -0600

    COOK-324, update wordpress attributes

--- wordpress/attributes/default.rb
+++ wordpress/attributes/default.rb
@@ -19,16 +19,16 @@
 #
 
 # General settings
-set_unless[:wordpress][:version] = "2.9.2"
-set_unless[:wordpress][:checksum] = "7342627f4a3dca44886c5aca6834cc88671dbd3aa2760182d2fcb9a330807ce1"
-set_unless[:wordpress][:dir] = "/var/www/wordpress"
-set_unless[:wordpress][:db][:database] = "wordpressdb"
-set_unless[:wordpress][:db][:user] = "wordpressuser"
+default[:wordpress][:version] = "2.9.2"
+default[:wordpress][:checksum] = "7342627f4a3dca44886c5aca6834cc88671dbd3aa2760182d2fcb9a330807ce1"
+default[:wordpress][:dir] = "/var/www/wordpress"
+default[:wordpress][:db][:database] = "wordpressdb"
+default[:wordpress][:db][:user] = "wordpressuser"
 
 ::Chef::Node.send(:include, Opscode::OpenSSL::Password)
 
-set_unless[:wordpress][:db][:password] = secure_password
-set_unless[:wordpress][:keys][:auth] = secure_password
-set_unless[:wordpress][:keys][:secure_auth] = secure_password
-set_unless[:wordpress][:keys][:logged_in] = secure_password
-set_unless[:wordpress][:keys][:nonce] = secure_password
+default[:wordpress][:db][:password] = secure_password
+default[:wordpress][:keys][:auth] = secure_password
+default[:wordpress][:keys][:secure_auth] = secure_password
+default[:wordpress][:keys][:logged_in] = secure_password
+default[:wordpress][:keys][:nonce] = secure_password
