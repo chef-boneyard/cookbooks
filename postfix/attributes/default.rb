44a0da586e239b775a976cb48db2dd0e7548e35a
jtimberman <joshua@opscode.com>
2010-07-10 21:18:32 -0600

    COOK-324, update postfix attributes

--- postfix/attributes/default.rb
+++ postfix/attributes/default.rb
@@ -1,14 +1,14 @@
-set_unless[:postfix][:mail_type]  = "client"
-set_unless[:postfix][:myhostname] = fqdn
-set_unless[:postfix][:mydomain]   = domain
-set_unless[:postfix][:myorigin]   = "$myhostname"
-set_unless[:postfix][:relayhost]  = ""
-set_unless[:postfix][:mail_relay_networks] = "127.0.0.0/8"
+default[:postfix][:mail_type]  = "client"
+default[:postfix][:myhostname] = fqdn
+default[:postfix][:mydomain]   = domain
+default[:postfix][:myorigin]   = "$myhostname"
+default[:postfix][:relayhost]  = ""
+default[:postfix][:mail_relay_networks] = "127.0.0.0/8"
 
-set_unless[:postfix][:smtp_sasl_auth_enable] = "no"
-set_unless[:postfix][:smtp_sasl_password_maps]    = "hash:/etc/postfix/sasl_passwd"
-set_unless[:postfix][:smtp_sasl_security_options] = "noanonymous"
-set_unless[:postfix][:smtp_tls_cafile] = "/etc/postfix/cacert.pem"
-set_unless[:postfix][:smtp_use_tls]    = "yes"
-set_unless[:postfix][:smtp_sasl_user_name] = ""
-set_unless[:postfix][:smtp_sasl_passwd]    = ""
+default[:postfix][:smtp_sasl_auth_enable] = "no"
+default[:postfix][:smtp_sasl_password_maps]    = "hash:/etc/postfix/sasl_passwd"
+default[:postfix][:smtp_sasl_security_options] = "noanonymous"
+default[:postfix][:smtp_tls_cafile] = "/etc/postfix/cacert.pem"
+default[:postfix][:smtp_use_tls]    = "yes"
+default[:postfix][:smtp_sasl_user_name] = ""
+default[:postfix][:smtp_sasl_passwd]    = ""
