4f252c4593b04e96815c90c549ffa00283a9e423
jtimberman <joshua@opscode.com>
2010-07-27 12:06:18 -0600

    COOK-328, add irb1.8 to debian/ubuntu

--- ruby/metadata.rb
+++ ruby/metadata.rb
@@ -2,7 +2,7 @@ maintainer        "Opscode, Inc."
 maintainer_email  "cookbooks@opscode.com"
 license           "Apache 2.0"
 description       "Installs ruby packages"
-version           "0.7.1"
+version           "0.7.2"
 
 recipe "ruby", "Installs ruby packages"
 
