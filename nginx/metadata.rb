2da871be74e4afa525ffc949c17e7b7a89727208
gilles <gilles@peerpong.com>
2010-08-12 09:37:30 -0700

    Revert "update metadata"
    This reverts commit 506fb38549bce9dfa35878b23c5790a6343d23f2.

--- nginx/metadata.rb
+++ nginx/metadata.rb
@@ -2,12 +2,12 @@ maintainer        "Opscode, Inc."
 maintainer_email  "cookbooks@opscode.com"
 license           "Apache 2.0"
 description       "Installs and configures nginx"
-version           "0.14.3"
+version           "0.14.2"
 
 recipe "nginx", "Installs nginx package and sets up configuration with Debian apache style with sites-enabled/sites-available"
 recipe "nginx::source", "Installs nginx from source and sets up configuration with Debian apache style with sites-enabled/sites-available"
 
-%w{ ubuntu debian centos redhat fedora}.each do |os|
+%w{ ubuntu debian }.each do |os|
   supports os
 end
 
