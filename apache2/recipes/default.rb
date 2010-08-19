d74e7b833763914389ea400f36f7a523308c3cb2
jtimberman <joshua@opscode.com>
2010-08-18 18:15:14 -0600

    add sleep for ubuntu/debian restart to avoid race

--- apache2/recipes/default.rb
+++ apache2/recipes/default.rb
@@ -38,6 +38,8 @@ service "apache2" do
     reload_command "/sbin/service httpd reload && sleep 1"
   when "debian","ubuntu"
     service_name "apache2"
+    restart_command "/usr/sbin/invoke-rc.d apache2 restart && sleep 1"
+    reload_command "/usr/sbin/invoke-rc.d apache2 reload && sleep 1"
   end
   supports value_for_platform(
     "debian" => { "4.0" => [ :restart, :reload ], "default" => [ :restart, :reload, :status ] },
