82c1408d6819db58ca09b9210e473d37f3d0f134
jtimberman <joshua@opscode.com>
2010-08-23 14:26:27 -0600

    COOK-348, update source recipe to install 1.0.1
    - need to handle platforms differently
    - ubuntu changed the javascript development package to xulrunner for
      lucid
    - hardy isn't supported and we should return with a log message (hardy
      doesn't have sufficient library versions to install couchdb 1.0.1)

--- couchdb/attributes/default.rb
+++ couchdb/attributes/default.rb
@@ -17,6 +17,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-set[:couch_db][:src_checksum] = "b16f3a10b406d01552893d356cdbf7f429209f938ae7d3aa589ee704df8cb4d2"
-set[:couch_db][:src_version] = "0.9.1"
+set[:couch_db][:src_checksum] = "b74a5bfe13e3457798107509cb533d0e7df276adad526ccd5a381c1271108b1f"
+set[:couch_db][:src_version] = "1.0.1"
 set[:couch_db][:src_mirror]  = "http://archive.apache.org/dist/couchdb/#{couch_db.src_version}/apache-couchdb-#{couch_db.src_version}.tar.gz"
