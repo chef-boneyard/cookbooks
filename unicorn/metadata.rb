4eed89d330f61430615820481366bdd54f41018f
jtimberman <joshua@opscode.com>
2010-08-25 13:53:44 -0600

    COOK-289, version update for sharing

--- unicorn/metadata.rb
+++ unicorn/metadata.rb
@@ -3,7 +3,7 @@ maintainer_email "ops@opscode.com"
 license          "Apache 2.0"
 description      "Installs/Configures unicorn"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-version          "0.1.2"
+version          "0.1.3"
 depends          "ruby"
 depends          "rubygems"
 recipe "unicorn", "Installs unicorn rubygem"
