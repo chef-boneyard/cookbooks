239c143946b80a37f34f50ca4955559af2cd775c
jtimberman <joshua@opscode.com>
2010-09-06 23:57:30 -0600

    perl version bump for release

--- perl/metadata.rb
+++ perl/metadata.rb
@@ -3,10 +3,10 @@ maintainer_email  "cookbooks@opscode.com"
 license           "Apache 2.0"
 description       "Installs perl and provides a define for maintaining CPAN modules"
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-version           "0.9.2"
+version           "0.10.0"
 
 recipe "perl", "Installs perl and provides a script to install cpan modules"
 
-%w{ ubuntu debian centos redhat }.each do |os|
+%w{ ubuntu debian centos redhat arch}.each do |os|
   supports os
 end
