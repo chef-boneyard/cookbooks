e550f024b7672f5aa5231c3d1f41e1726400fbf6
jtimberman <joshua@opscode.com>
2010-09-06 23:32:58 -0600

    Add pacman cookbook

---
+++ pacman/metadata.rb
@@ -0,0 +1,9 @@
+maintainer       "Opscode, Inc."
+maintainer_email "cookbooks@opscode.com"
+license          "Apache 2.0"
+description      "Updates package list for pacman and has LWRP for pacman groups"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.9.0"
+supports         "arch"
+
+recipe "pacman", "Refreshes the pacman package cache"
