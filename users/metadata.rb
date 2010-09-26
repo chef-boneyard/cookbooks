94a18bc68e3a0a3bf41ac3b09928de6e178e6fde
Darrin Eden <darrin.eden@gmail.com>
2010-09-26 13:29:35 -0700

    sysadmin depends on sudo

--- users/metadata.rb
+++ users/metadata.rb
@@ -4,3 +4,4 @@ license          "Apache 2.0"
 description      "Creates users from a databag search"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
 version          "0.2.0"
+depends          "sudo"
