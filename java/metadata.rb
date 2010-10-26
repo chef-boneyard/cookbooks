cbd222e3fcac5c34bbfb861ef5b853609159d20d
Seth Chisamore <schisamo@opscode.com>
2010-10-25 23:49:31 -0400

    first pass at a combined openjdk and sun java cookbook

--- java/metadata.rb
+++ java/metadata.rb
@@ -2,11 +2,10 @@ maintainer        "Opscode, Inc."
 maintainer_email  "cookbooks@opscode.com"
 license           "Apache 2.0"
 description       "Installs java via openjdk."
-long_description  IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
+long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version           "0.10.2"
-
-recipe "java", "Installs openjdk to provide Java"
-
+depends "apt"
 %w{ debian ubuntu redhat centos fedora }.each do |os|
   supports os
 end
+recipe "java", "Installs openjdk to provide Java"
--- java/recipes/default.rb
+++ java/recipes/default.rb
@@ -2,7 +2,7 @@
 # Cookbook Name:: java
 # Recipe:: default
 #
-# Copyright 2008-2009, Opscode, Inc.
+# Copyright 2008-2010, Opscode, Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -16,17 +16,48 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+include_recipe "java::#{node["java"]["install_flavor"]}"
 
-java_pkg = value_for_platform(
-  [ "ubuntu", "debian" ] => {
-    "default" => "openjdk-6-jre-headless"
-  },
-  [ "redhat", "centos", "fedora" ] => {
-    "default" => "java-1.6.0-openjdk"
-  },
-  "default" => "openjdk-6-jre-headless"
-)
+current_java_version_pattern = (node.java.install_flavor == 'sun') ? /Java HotSpot\(TM\)/ : /^OpenJDK/
 
-package java_pkg do
-  action :install
+# force ohai to run and pick up new languages.java data
+ruby_block "reload_ohai" do
+  block do
+    o = Ohai::System.new
+    o.all_plugins
+    node.automatic_attrs.merge! o.data
+  end
+  action :nothing
 end
+
+execute "update-java-alternatives" do
+  command "update-java-alternatives --jre -s java-6-#{node["java"]["install_flavor"]}"
+  returns 0
+  only_if do platform?("ubuntu", "debian") end
+  action :nothing
+  notifies :create, resources(:ruby_block => "reload_ohai")
+end
+
+node.run_state[:java_pkgs].each do |pkg|
+  package pkg do
+    action :install
+    if platform?("ubuntu", "debian")
+      if node.java.install_flavor == "sun"
+        response_file "java.seed"
+      end
+      notifies :run, resources(:execute => "update-java-alternatives"), :delayed
+    end
+  end
+end
+
+# re-run update-java-alternatives if our java flavor changes
+if node.languages.attribute?("java")
+  unless node.languages.java.hotspot.name.match(current_java_version_pattern)
+    log "Java install_flavor has changed, re-running 'update-java-alternatives'" do
+      level :info
+      notifies :run, resources(:execute => "update-java-alternatives"), :delayed
+    end
+  end
+end
+
+node.run_state.delete(:java_pkgs)
