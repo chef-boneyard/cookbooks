8ca786fb3b04c5f1291faace616a275d69b95c05
Seth Chisamore <schisamo@opscode.com>
2010-12-10 12:34:29 -0500

    bumped versions and generated metadata for release

--- java/metadata.rb
+++ java/metadata.rb
@@ -3,7 +3,7 @@ maintainer_email  "cookbooks@opscode.com"
 license           "Apache 2.0"
 description       "Installs java via openjdk."
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           "0.10.2"
+version           "0.10.3"
 depends "apt"
 %w{ debian ubuntu centos redhat fedora }.each do |os|
   supports os
