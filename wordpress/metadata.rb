ce5714681d64c9ae23afd8a6d1c8fd36a325666b
jtimberman <joshua@opscode.com>
2010-12-12 22:51:49 -0800

    update wordpress cookbook to install wp version 3.0.3

--- wordpress/metadata.rb
+++ wordpress/metadata.rb
@@ -3,7 +3,7 @@ maintainer_email "cookbooks@opscode.com"
 license          "Apache 2.0"
 description      "Installs/Configures wordpress"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-version          "0.6.3"
+version          "0.7.0"
 depends          "php"
 depends          "apache2"
 depends          "mysql"
