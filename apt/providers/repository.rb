bc80cf5ea017f05220c9660920b92234c98365ea
mattray <matthewhray@gmail.com>
2010-12-18 09:51:19 -0600

    updated version number, check for existence of repository

--- apt/providers/repository.rb
+++ apt/providers/repository.rb
@@ -1,24 +1,26 @@
 action :add do
-  Chef::Log.info "Adding #{new_resource.repo_name} repository to /etc/apt/sources.list.d/#{new_resource.repo_name}-source.list"
-  #build our listing
-  repository = "deb"
-  repository = "deb-src" if new_resource.deb_src
-  repository = "#Created by the Chef apt_repository LWRP\n" + repository
-  repository += " #{new_resource.uri}"
-  repository += " #{new_resource.distribution}"
-  new_resource.components.each {|component| repository += " #{component}"}
-  #write out the file, replace it if it already exists
-  file "/etc/apt/sources.list.d/#{new_resource.repo_name}-source.list" do
-    owner "root"
-    group "root"
-    mode 0644
-    content repository + "\n"
-    action :create
+  unless ::File.exists?("/etc/apt/sources.list.d/#{new_resource.repo_name}-source.list")
+    Chef::Log.info "Adding #{new_resource.repo_name} repository to /etc/apt/sources.list.d/#{new_resource.repo_name}-source.list"
+    #build our listing
+    repository = "deb"
+    repository = "deb-src" if new_resource.deb_src
+    repository = "#Created by the Chef apt_repository LWRP\n" + repository
+    repository += " #{new_resource.uri}"
+    repository += " #{new_resource.distribution}"
+    new_resource.components.each {|component| repository += " #{component}"}
+    #write out the file, replace it if it already exists
+    file "/etc/apt/sources.list.d/#{new_resource.repo_name}-source.list" do
+      owner "root"
+      group "root"
+      mode 0644
+      content repository + "\n"
+      action :create
+    end
+    execute "apt-get update" do
+      action :run
+    end
   end
-  execute "apt-get update" do
-    action :run
-  end
-end
+end 
 
 action :remove do
   Chef::Log.info "Removing #{new_resource.repo_name} repository from /etc/apt/sources.list.d/"
