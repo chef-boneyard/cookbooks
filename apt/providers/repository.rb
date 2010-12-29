0b51bb030c9ecf0f2db632a8c3985d7383cbc9a2
Seth Chisamore <schisamo@opscode.com>
2010-12-29 15:12:05 -0500

    added updated_by_last_action flags to add/remove actions

--- apt/providers/repository.rb
+++ apt/providers/repository.rb
@@ -1,14 +1,14 @@
 action :add do
   unless ::File.exists?("/etc/apt/sources.list.d/#{new_resource.repo_name}-source.list")
     Chef::Log.info "Adding #{new_resource.repo_name} repository to /etc/apt/sources.list.d/#{new_resource.repo_name}-source.list"
-    #build our listing
+    # build our listing
     repository = "deb"
     repository = "deb-src" if new_resource.deb_src
-    repository = "#Created by the Chef apt_repository LWRP\n" + repository
+    repository = "# Created by the Chef apt_repository LWRP\n" + repository
     repository += " #{new_resource.uri}"
     repository += " #{new_resource.distribution}"
     new_resource.components.each {|component| repository += " #{component}"}
-    #write out the file, replace it if it already exists
+    # write out the file, replace it if it already exists
     file "/etc/apt/sources.list.d/#{new_resource.repo_name}-source.list" do
       owner "root"
       group "root"
@@ -16,16 +16,21 @@ action :add do
       content repository + "\n"
       action :create
     end
-    execute "apt-get update" do
+    e = execute "update package index" do
+      command "apt-get update"
       action :run
     end
+    e.run_action(:run)
+    new_resource.updated_by_last_action(true)
   end
 end 
 
 action :remove do
-  Chef::Log.info "Removing #{new_resource.repo_name} repository from /etc/apt/sources.list.d/"
-  file "/etc/apt/sources.list.d/#{new_resource.repo_name}-source.list" do
-    action :delete
+  if ::File.exists?("/etc/apt/sources.list.d/#{new_resource.repo_name}-source.list")
+    Chef::Log.info "Removing #{new_resource.repo_name} repository from /etc/apt/sources.list.d/"
+    file "/etc/apt/sources.list.d/#{new_resource.repo_name}-source.list" do
+      action :delete
+    end
+    new_resource.updated_by_last_action(true)
   end
 end
-
