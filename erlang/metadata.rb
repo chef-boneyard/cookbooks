2dac45c322816d3404d916f91ee9064fe4a8194a
Eric Coleman <eric@colemando.com>
2011-02-09 01:17:01 -0500

    erlang support for archlinux

--- erlang/metadata.rb
+++ erlang/metadata.rb
@@ -2,10 +2,10 @@ maintainer        "Opscode, Inc."
 maintainer_email  "cookbooks@opscode.com"
 license           "Apache 2.0"
 description       "Installs erlang, optionally install GUI tools."
-version           "0.8.2"
+version           "0.8.3"
 
 recipe "erlang", "Installs erlang"
 
-%w{ ubuntu debian }.each do |os|
+%w{ ubuntu debian arch }.each do |os|
   supports os
 end
