39feb07bc92c26af58f1a3645448ca00f21d9641
Seth Chisamore <schisamo@opscode.com>
2011-02-17 22:11:50 -0500

    python v1.0.1, made pip version check pattern more strict in load_current_resource.

--- python/metadata.rb
+++ python/metadata.rb
@@ -2,7 +2,7 @@ maintainer        "Opscode, Inc."
 maintainer_email  "cookbooks@opscode.com"
 license           "Apache 2.0"
 description       "Installs python packages. Includes LWRPs for managing `pip` packages and `virtualenv` isolated Python environments."
-version           "1.0.0"
+version           "1.0.1"
 
 recipe "python", "Installs python, pip, and virtualenv"
 
