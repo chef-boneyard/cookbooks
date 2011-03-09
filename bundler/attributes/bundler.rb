a77b587037a5b602bdd8804fe763552f9f84ec0c
mattray <matthewhray@gmail.com>
2011-03-08 20:18:03 -0600

    sync

--- bundler/attributes/bundler.rb
+++
@@ -1,1 +0,0 @@
-set_unless[:bundler][:apps_path] = "/home/ubuntu/apps"
--- bundler/recipes/install.rb
+++
@@ -1,4 +0,0 @@
-bash "run bundle install in app directory" do
-  cwd File.join(node[:bundler][:apps_path], node[:bundler][:app], "current")
-  code "bundle install"
-end
