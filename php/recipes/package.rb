01e6f2234433f58098c5bcf889b6fbf44e8432af
Seth Chisamore <schisamo@opscode.com>
2011-03-16 16:22:29 -0400

    [COOK-475] PHP cookbook improvements

--- php/recipes/package.rb
+++ php/recipes/package.rb
@@ -1,9 +1,9 @@
 #
-# Author::  Joshua Timberman (<joshua@opscode.com>)
+# Author::  Seth Chisamore (<schisamo@opscode.com>)
 # Cookbook Name:: php
-# Recipe:: module_curl
+# Recipe:: package
 #
-# Copyright 2009, Opscode, Inc.
+# Copyright 2011, Opscode, Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -18,11 +18,22 @@
 # limitations under the License.
 #
 
-case node[:platform]
-  when "centos", "redhat", "fedora", "suse"
-    #centos php compiled with curl
-  when "debian", "ubuntu"
-    package "php5-curl" do
-      action :upgrade
-    end
+php_pkgs = value_for_platform(
+  [ "centos", "redhat", "fedora" ] => {
+    "default" => %w{php php-devel php-cli php-pear}
+  },
+  "default" => %w{php5 php5-dev php5-cli php-pear}
+)
+
+php_pkgs.each do |pkg|
+  package pkg do
+    action :upgrade
+  end
+end
+
+template "#{node['php']['conf_dir']}/php.ini" do
+  source "php.ini.erb"
+  owner "root"
+  group "root"
+  mode "0644"
 end
