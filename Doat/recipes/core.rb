6f5a8e75b692ad99f1102f18142772b6d7fd4716
Avishai Ish-Shalom <avishai.ish-shalom@mail.huji.ac.il>
2011-03-18 19:01:23 +0200

    Added python dependencies, added missing variables for templates Conflicts:
    	Doat/recipes/core.rb

--- Doat/recipes/core.rb
+++ Doat/recipes/core.rb
@@ -1,11 +1,15 @@
 include_recipe "Doat"
 include_recipe "redis"
 include_recipe "Doat::scribe-client"
+include_recipe "python"
 
-package "python-crypto"
-package "python-nltk"
-easy_install_package "PySQLPool"
-easy_install_package "Crypto.Ciper"
+%w(python-crypto python-nltk python-mysqldb python-enchant).each do |pkg|
+  package pkg
+end
+
+%w(PySQLPool redis hiredis).each do |pkg|
+  easy_install_package pkg
+end
 
 redis_instance "melt" do
   data_dir "/var/lib/redis/melt"
@@ -22,13 +26,23 @@ link "/etc/init.d/cored" do
 end
 
 link "/etc/init.d/autocompleted" do
-  "/opt/doat/etc/scripts/autocompleted"
+  to "/opt/doat/etc/scripts/autocompleted"
 end
 
-redis_master = provider_for_service(:redis_melt, :filters => {:replication => "master"})
+if node[:redis][:instances][:melt][:replication][:role] == "master"
+  redis_melt_master = node
+else
+  redis_melt_master = provider_for_service(:redis_melt, :filters => {:replication => "master"})
+end
+app_config = data_bag_item(:doat_config, :core)
+sql_host = search(:endpoints, "type:rds AND db:#{app_config["db"]}").first
+sql_credentials = search(:credentials, "usage:db_#{app_config["db"]}").first
+
 template "/opt/doat/etc/servers/core/core.conf" do
   source "core.conf.erb"
-  variables :redis_master => redis_master, :redis_search_node => node
+  variables :redis_melt_master => redis_melt_master, :redis_melt_slave => node, :redis_search_node => node,
+    :sql_credentials => sql_credentials, :sql => sql_host, :autocomplete_node => node,
+    :app_config => app_config
   notifies :restart, "service[cored]"
 end
 
