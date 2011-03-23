af3460b7b0a24f09139cc124d5157f87ba4e4363
Nuo Yan <nuo@opscode.com>
2011-03-23 15:46:36 -0700

    add gecode cookbook and update chef server bootstrap to install gecode

---
+++ gecode/metadata.rb
@@ -0,0 +1,7 @@
+maintainer       "YOUR_COMPANY_NAME"
+maintainer_email "YOUR_EMAIL"
+license          "All rights reserved"
+description      "Installs/Configures gecode"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
+version          "0.0.1"
+depends          "apt"
