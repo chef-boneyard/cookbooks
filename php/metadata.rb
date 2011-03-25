77ef175473b4eabc75e571825bb9c083824fc83d
Seth Chisamore <schisamo@opscode.com>
2011-03-25 14:40:07 -0400

    php v0.99.2, re-released with deprecated module_* recipes

--- php/metadata.rb
+++ php/metadata.rb
@@ -2,7 +2,7 @@ maintainer        "Opscode, Inc."
 maintainer_email  "cookbooks@opscode.com"
 license           "Apache 2.0"
 description       "Installs and maintains php and php modules"
-version           "0.99.1"
+version           "0.99.2"
 
 depends "build-essential"
 depends "xml"
