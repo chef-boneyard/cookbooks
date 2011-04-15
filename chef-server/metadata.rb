8b4cdd8ef13b1d84f20ce7cceef35bb76466bf9d
jtimberman <joshua@opscode.com>
2011-04-15 01:09:37 -0600

    Refactoring chef -> chef-server cookbook
    * renaming 'chef' to 'chef-server'
    * change attributes in cookbook
    * use rabbitmq::chef
    * chef-solr-indexer is now chef-expander
    * update README

---
+++ chef-server/metadata.rb
@@ -0,0 +1,17 @@
+maintainer        "Opscode, Inc."
+maintainer_email  "cookbooks@opscode.com"
+license           "Apache 2.0"
+description       "Installs and configures Chef Server"
+long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version           "0.99.10"
+recipe            "chef_server", "Compacts the Chef Server CouchDB."
+recipe            "chef_server::rubygems-install", "Set up rubygem installed chef server."
+recipe            "chef_server::apache-proxy", "Configures Apache2 proxy for API and WebUI"
+
+%w{ ubuntu debian redhat centos fedora freebsd openbsd }.each do |os|
+  supports os
+end
+
+%w{ runit bluepill daemontools couchdb rabbitmq_chef apache2 openssl zlib xml java gecode }.each do |cb|
+  depends cb
+end
