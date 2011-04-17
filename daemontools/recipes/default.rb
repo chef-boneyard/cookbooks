9d79eb1e9a64b9f7bd52cba69c3a42652c9b8c68
Jay Buffington <jaybuffington@gmail.com>
2011-04-17 13:55:15 -0700

    start svscan for daemontools on debian/ubuntu

--- daemontools/recipes/default.rb
+++ daemontools/recipes/default.rb
@@ -68,3 +68,11 @@ when "source"
 else
   Chef::Log.info("Could not find a method to install Daemon Tools for platform #{node[:platform]}, version #{node[:platform_version]}")
 end
+
+service "svscan" do
+    provider Chef::Provider::Service::Upstart
+    enabled true
+    running true
+    supports :restart => true, :reload => true, :status => true
+    action [:enable, :start]
+end
