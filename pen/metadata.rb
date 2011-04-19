0e418d5ad21fb4a7daeef90a94f66ed0996602b9
Avishai Ish-Shalom <avishai.ish-shalom@mail.huji.ac.il>
2011-04-19 23:39:14 +0300

    Added some documentation

--- pen/metadata.rb
+++ pen/metadata.rb
@@ -4,3 +4,6 @@ license          "Apache 2.0"
 description      "Installs/Configures pen"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
 version          "0.1"
+
+supports         "debian"
+supports         "ubuntu"
