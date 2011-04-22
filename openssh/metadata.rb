4833028767bcbb2f94495e92dbd8430522b05a8f
mattray <matthewhray@gmail.com>
2011-04-22 17:40:01 -0500

    added Scientific Linux support

--- openssh/metadata.rb
+++ openssh/metadata.rb
@@ -2,10 +2,10 @@ maintainer        "Opscode, Inc."
 maintainer_email  "cookbooks@opscode.com"
 license           "Apache 2.0"
 description       "Installs openssh"
-version           "0.8.0"
+version           "0.8.1"
 
 recipe "openssh", "Installs openssh"
 
-%w{ redhat centos fedora ubuntu debian arch}.each do |os|
+%w{ redhat centos fedora ubuntu debian arch scientific }.each do |os|
   supports os
 end
