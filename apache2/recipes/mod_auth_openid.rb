1599c916520a91b8f221fc0235473f2d332bc058
Jim Browne <jbrowne@42lines.net>
2011-04-24 12:24:58 -0700

    [COOK-519] Introduces attribute node[:apache][:lib_dir] which handles lib vs. lib64 on RHEL/CentOS and removes case statements for other platforms.
    Builds libopkele and mod_auth_openid on RHEL/CentOS.
    
    Expects use of EPEL repository on RHEL/CentOS for packages libtidy and
    libtidy-devel.
    
    As @DEVOPS_BORAT said: "For rainy afternoon when life is seem boring, I
    recommend try install new version of anything on CentOS."

--- apache2/recipes/mod_auth_openid.rb
+++ apache2/recipes/mod_auth_openid.rb
@@ -18,8 +18,12 @@
 #
 
 openid_dev_pkgs = value_for_platform(
-  "ubuntu" => { "default" => %w{ apache2-prefork-dev libopkele-dev libopkele3 } },
+  "ubuntu" => { "default" => %w{ g++ apache2-prefork-dev libopkele-dev libopkele3 } },
   "debian" => { "default" => %w{ apache2-prefork-dev libopkele-dev libopkele3 } },
+  # libtidy and libtidy-devel are in EPEL repository
+  "redhat" => { "default" => %w{ gcc-c++ httpd-devel curl-devel libtidy libtidy-devel } },
+  # libtidy and libtidy-devel are in EPEL repository
+  "centos" => { "default" => %w{ gcc-c++ httpd-devel curl-devel libtidy libtidy-devel } },
   "arch" => { "default" => ["libopkele"] }
 )
 
@@ -40,6 +44,26 @@ openid_dev_pkgs.each do |pkg|
   end
 end
 
+case node[:platform]
+when "centos","redhat"
+  remote_file "#{Chef::Config[:file_cache_path]}/libopkele-2.0.4.tar.gz" do
+    source "http://kin.klever.net/dist/libopkele-2.0.4.tar.gz"
+    mode 0644
+  end
+
+  bash "install libopkele" do
+    cwd "#{Chef::Config[:file_cache_path]}"
+    # Ruby 1.8.6 does not have rpartition, unfortunately
+    syslibdir = node[:apache][:lib_dir][0..node[:apache][:lib_dir].rindex("/")]
+    code <<-EOH
+    tar zxvf libopkele-2.0.4.tar.gz
+    cd libopkele-2.0.4.tar && ./configure --prefix=/usr --libdir=#{syslibdir}
+    make && make install
+    EOH
+    not_if { File.exists?("#{syslibdir}/libopkele.a") }
+  end
+end
+
 remote_file "#{Chef::Config[:file_cache_path]}/mod_auth_openid-0.4.tar.gz" do
   source "http://butterfat.net/releases/mod_auth_openid/mod_auth_openid-0.4.tar.gz"
   mode 0644
@@ -53,12 +77,7 @@ bash "install mod_auth_openid" do
   perl -pi -e "s/-i -a -n 'authopenid'/-i -n 'authopenid'/g" Makefile
   make && make install
   EOH
-  case node[:platform]
-  when "arch"
-    not_if { ::File.exists?("/usr/lib/httpd/modules/mod_auth_openid.so") }
-  else
-    not_if { ::File.exists?("/usr/lib/apache2/modules/mod_auth_openid.so") }
-  end
+  not_if { ::File.exists?("#{node[:apache][:lib_dir]}/modules/mod_auth_openid.so") }
 end
 
 file "#{node[:apache][:cache_dir]}/mod_auth_openid.db" do
