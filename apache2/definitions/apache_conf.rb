a1d8f981eb8fa9b1973f878710e5b74b844af672
Andrea Campi <andrea.campi@zephirworks.com>
2011-05-08 15:25:38 +0200

    [COOK-549] specify the permissions for files created by apache_conf.

--- apache2/definitions/apache_conf.rb
+++ apache2/definitions/apache_conf.rb
@@ -20,6 +20,7 @@
 define :apache_conf do
   template "#{node[:apache][:dir]}/mods-available/#{params[:name]}.conf" do
     source "mods/#{params[:name]}.conf.erb"
+    mode 0644
     notifies :restart, resources(:service => "apache2")
     mode 0644
   end
