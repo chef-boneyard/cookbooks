e98faab6895773d94d702779a33a9e770b88940c
Nathan L Smith <nlloyds@gmail.com>
2011-05-11 16:21:34 -0500

    Don't use devices as name attribute for zenbatchload

--- zenoss/resources/zenbatchload.rb
+++ zenoss/resources/zenbatchload.rb
@@ -1,5 +1,6 @@
 actions :run
 
-attribute :devices, :kind_of => Hash, :default => {}, :name_attribute => true
+attribute :name, :kind_of => String, :default => '', :name_attribute => true
+attribute :devices, :kind_of => Hash, :default => {}
 attribute :locations, :kind_of => Array, :default => []
 attribute :groups, :kind_of => Array, :default => []
