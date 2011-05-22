81d881870c8e909da082fe8f9ae9cf074ae7043a
jtimberman <joshua@opscode.com>
2011-05-21 20:58:57 -0600

    COOK-574, use dashes instead of underscores in recipe names

--- chef-server/metadata.rb
+++ chef-server/metadata.rb
@@ -4,9 +4,9 @@ license           "Apache 2.0"
 description       "Installs and configures Chef Server"
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version           "0.99.11"
-recipe            "chef_server", "Compacts the Chef Server CouchDB."
-recipe            "chef_server::rubygems-install", "Set up rubygem installed chef server."
-recipe            "chef_server::apache-proxy", "Configures Apache2 proxy for API and WebUI"
+recipe            "chef-server", "Compacts the Chef Server CouchDB."
+recipe            "chef-server::rubygems-install", "Set up rubygem installed chef server."
+recipe            "chef-server::apache-proxy", "Configures Apache2 proxy for API and WebUI"
 
 %w{ ubuntu debian redhat centos fedora freebsd openbsd }.each do |os|
   supports os
