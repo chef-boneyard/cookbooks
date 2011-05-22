7fb8a24abf47127832ef77cd6b63a45f1aa3af02
jtimberman <joshua@opscode.com>
2011-05-21 21:21:47 -0600

    install rake 0.8.7 for radiant

--- radiant/recipes/default.rb
+++ radiant/recipes/default.rb
@@ -27,6 +27,10 @@ include_recipe "apache2"
 include_recipe "apache2::mod_rewrite"
 include_recipe "passenger_apache2::mod_rails"
 
+gem_package "rake" do
+  version "0.8.7"
+end
+
 gem_package "sqlite3-ruby"
 
 group "railsdev"
