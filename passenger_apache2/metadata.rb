5eac6f89d592ec4ea9f1db5449f3c0c43fe03125
jtimberman <joshua@opscode.com>
2011-05-21 21:53:58 -0600

    passenger depend on build-essential

--- passenger_apache2/metadata.rb
+++ passenger_apache2/metadata.rb
@@ -9,6 +9,7 @@ recipe "passenger_apache2", "Installs Passenger as an Apache module"
 recipe "passenger_apache2::mod_rails", "Enables Apache module configuration for passenger module"
 
 depends "apache2"
+depends "build-essential"
 
 %w{ redhat centos ubuntu debian }.each do |os|
   supports os
