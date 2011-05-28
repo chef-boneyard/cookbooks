51c8c023b7dbb261111f2bdaa2417a18d218e2c3
mattray <matthewhray@gmail.com>
2011-05-28 00:12:01 -0500

    substantial update to rabbitmq for COOK-582 (cherry picked from commit 3628d26b4e1ce42b8b1ea440bdefc68d453f2746)

--- rabbitmq/attributes/default.rb
+++ rabbitmq/attributes/default.rb
@@ -1,12 +1,10 @@
-default[:rabbitmq][:nodename]  = "rabbit"
-default[:rabbitmq][:address]  = "0.0.0.0"
-default[:rabbitmq][:port]  = "5672"
-default[:rabbitmq][:erl_args]  = "+K true +A 30 \
--kernel inet_default_listen_options [{nodelay,true},{sndbuf,16384},{recbuf,4096}] \
--kernel inet_default_connect_options [{nodelay,true}]"
-default[:rabbitmq][:start_args] = ""
-default[:rabbitmq][:logdir] = "/var/log/rabbitmq"
-default[:rabbitmq][:mnesiadir] = "/var/lib/rabbitmq/mnesia"
+default[:rabbitmq][:nodename]  = node[:hostname]
+default[:rabbitmq][:address]  = nil
+default[:rabbitmq][:port]  = nil
+default[:rabbitmq][:config] = nil
+default[:rabbitmq][:logdir] = nil
+default[:rabbitmq][:mnesiadir] = nil
+#clustering
 default[:rabbitmq][:cluster] = "no"
 default[:rabbitmq][:cluster_config] = "/etc/rabbitmq/rabbitmq_cluster.config"
 default[:rabbitmq][:cluster_disk_nodes] = []
--- rabbitmq/metadata.rb
+++ rabbitmq/metadata.rb
@@ -1,10 +1,11 @@
-maintainer        "Benjamin Black"
-maintainer_email  "b@b3k.us"
+maintainer        "Opscode, Inc."
+maintainer_email  "cookbooks@opscode.com"
 license           "Apache 2.0"
 description       "Installs and configures RabbitMQ server"
-version           "0.3"
+version           "1.1"
 recipe            "rabbitmq", "Install and configure RabbitMQ"
 recipe            "rabbitmq::cluster", "Set up RabbitMQ clustering."
+depends           "apt", "> 1.1"
 
 %w{ubuntu debian}.each do |os|
   supports os
