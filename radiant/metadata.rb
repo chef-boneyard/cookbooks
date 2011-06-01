ad4828f969da2f248e968ee6436f5b3764d41fb5
Seth Chisamore <schisamo@opscode.com>
2011-06-01 14:52:28 -0400

    radiant v1.0.1
    * fix bad attribute file

--- radiant/metadata.rb
+++ radiant/metadata.rb
@@ -3,7 +3,7 @@ maintainer_email  "cookbooks@opscode.com"
 license           "Apache 2.0"
 description       "Installs radiant from Git repository"
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           "1.0.0"
+version           "1.0.1"
 
 recipe "radiant", "Installs Radiant CMS"
 recipe "radiant::db_bootstrap", "Bootstrap the Radiant database, used with application cookbook (destructive)"
