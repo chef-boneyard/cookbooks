cd49e8123749cb71c6e3dd033f28884484ec5481
Eric G. Wolfe <wolfe21@marshall.edu>
2011-06-02 12:53:54 -0400

    Add descriptive comment to sysadmin recipe

--- users/recipes/sysadmins.rb
+++ users/recipes/sysadmins.rb
@@ -18,6 +18,8 @@
 # limitations under the License.
 #
 
+# Searches data bag "users" for groups attribute "sysadmin".
+# Places returned users in Unix group "sysadmin" with GID 2300.
 users_manage "sysadmin" do
   group_id 2300
   action [ :remove, :create ]
