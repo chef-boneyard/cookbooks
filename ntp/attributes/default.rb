89e65d35f117983cebfbf3ccdd50fdc57e4ca667
Eric G. Wolfe <wolfe21@marshall.edu>
2011-06-06 21:14:16 -0400

    [COOK-376]  * Rebased via merge with opscode/master  * add iburst option for peers and upstream servers  * add fix-up for vmware system's without real-time CMOS clocks  * add restriction attribute  * add peer options for local NTP servers  * add directory creation to default recipe for RHEL/CentOS/Scientific edge-case  * updated README documentation  * bump cookbook version to 1.2.0

--- ntp/attributes/default.rb
+++ ntp/attributes/default.rb
@@ -6,4 +6,6 @@ when "redhat","centos","fedora","scientific"
 end
 
 default[:ntp][:is_server] = false
-default[:ntp][:servers]   = ["0.pool.ntp.org", "1.pool.ntp.org"]
+default[:ntp][:servers]   = ["0.pool.ntp.org", "1.pool.ntp.org", "2.pool.ntp.org", "3.pool.ntp.org"]
+default[:ntp][:peers] = []
+default[:ntp][:restrictions] = []
