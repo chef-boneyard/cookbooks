122b0a5a12fb1aef7113fbbf8ae32562eb08ea1a
Seth Chisamore <schisamo@opscode.com>
2011-06-07 21:30:27 -0400

    iptables v0.9.3, [COOK-580] iptables cookbook robustness

--- iptables/metadata.rb
+++ iptables/metadata.rb
@@ -2,7 +2,7 @@ maintainer        "Opscode, Inc."
 maintainer_email  "cookbooks@opscode.com"
 license           "Apache 2.0"
 description       "Sets up iptables to use a script to maintain rules"
-version           "0.9.2"
+version           "0.9.3"
 
 recipe "iptables", "Installs iptables and sets up .d style config directory of iptables rules"
 %w{ redhat centos debian ubuntu}.each do |os|
