4a843ed70d52edf648f7463dafd74ceef239d0dc
mattray <matthewhray@gmail.com>
2011-06-26 14:33:53 -0500

    moving to supporting multiple images

--- pxe_dust/metadata.rb
+++ pxe_dust/metadata.rb
@@ -1,9 +1,9 @@
 maintainer       "Opscode, Inc."
 maintainer_email "cookbooks@opscode.com"
 license          "Apache 2.0"
-description      "Configures installing Ubuntu with preseed files via PXE booting."
+description      "Configures installing operating systems with preseed and kickstart files via PXE booting."
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "1.2"
+version          "1.3"
 depends          "apache2"
 depends          "tftp"
 
