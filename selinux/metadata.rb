5b62fb29f0cfd7f2442af86d61e5874a92d21327
Sean OMeara <someara@opscode.com>
2011-08-04 16:17:59 -0400

    [COOK-678] - Adding selinux cookbook

---
+++ selinux/metadata.rb
@@ -0,0 +1,6 @@
+maintainer       "Opscode, Inc."
+maintainer_email "someara@opscode.com"
+license          "Apache"
+description      "Installs/Configures selinux"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
+version          "0.1.0"
---
+++ selinux/recipes/disabled.rb
@@ -0,0 +1,34 @@
+#
+# Author:: Sean OMeara (<someara@opscode.com>)
+# Cookbook Name:: selinux
+# Recipe:: disabled
+#
+# Copyright 2011, Opscode, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+execute "disable selinux enforcement" do
+  only_if "selinuxenabled"
+  command "setenforce 0"
+  action :run
+end
+
+template "/etc/sysconfig/selinux" do
+  source "sysconfig/selinux.erb"
+  variables(
+    :selinux => "disabled",
+    :selinuxtype => "targeted",
+  )
+end
+
