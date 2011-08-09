660e4277fe647ef5455f8158c8e4a437abc931c8
Seth Chisamore <schisamo@opscode.com>
2011-08-09 11:19:45 -0400

    fixed typo in metadata attribute name

--- sudo/metadata.rb
+++ sudo/metadata.rb
@@ -15,24 +15,24 @@ attribute "authorization",
   :description => "Hash of Authorization attributes",
   :type => "hash"
 
-attribute "authorization/sudoers",
+attribute "authorization/sudo",
   :display_name => "Authorization Sudoers",
-  :description => "Hash of Authorization/Sudoers attributes",
+  :description => "Hash of Authorization/Sudo attributes",
   :type => "hash"
 
-attribute "authorization/sudoers/users",
+attribute "authorization/sudo/users",
   :display_name => "Sudo Users",
   :description => "Users who are allowed sudo ALL",
   :type => "array",
   :default => ""
 
-attribute "authorization/sudoers/groups",
+attribute "authorization/sudo/groups",
   :display_name => "Sudo Groups",
   :description => "Groups who are allowed sudo ALL",
   :type => "array",
   :default => ""
 
-attribute "authorization/sudoers/passwordless",
+attribute "authorization/sudo/passwordless",
   :display_name => "Passwordless Sudo",
   :description => "",
   :type => "string",
