bd0633e34fa3addd7be3fba9028baf5395e8dd66
George Miroshnykov <george.miroshnykov@gmail.com>
2011-08-19 14:35:01 +0300

    Removed buggy windows cookbook.

--- windows/metadata.rb
+++
@@ -1,7 +0,0 @@
-maintainer       "Opscode, Inc."
-maintainer_email "cookbooks@opscode.com"
-license          "Apache 2.0"
-description      "Provides a set of useful Windows-specific primitives."
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "1.0.6"
-supports         "windows"
