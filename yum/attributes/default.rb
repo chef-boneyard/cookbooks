a4b7c3fa2493264a7ef702ae677b5bfc9308a9a2
jtimberman <joshua@opscode.com>
2011-08-22 23:46:19 -0600

    use ints for version

--- yum/attributes/default.rb
+++ yum/attributes/default.rb
@@ -24,10 +24,10 @@ default[:yum][:installonlypkgs]
 
 
 default['yum']['epel_release'] = case node['platform_version'].to_i
-                                  when "6"
+                                  when 6
                                     "6-5"
-                                  when "5"
+                                  when 5
                                     "5-4"
-                                  when "4"
+                                  when 4
                                     "4-10"
                                   end
