3199737140a25e26228e61b5cdcec8aff5d3ae7e
Paul Morton <pmorton@biaprotect.com>
2011-08-22 17:10:35 -0700

    COOK-717 - Removed windows_env_vars and moved path to use env provider (cherry picked from commit 86143e6184ea02d577bad6e9d5941e336fae358c)
    Conflicts:
    
    	windows/providers/env_vars.rb
    	windows/providers/path.rb

--- windows/providers/path.rb
+++ windows/providers/path.rb
@@ -18,37 +18,20 @@
 # limitations under the License.
 #
 
-action :add do
-  path = ENV['PATH'].split(';')
-  if !path.include?(new_resource.path)
-    path.insert(-1,new_resource.path)
-  end
 
-  windows_registry Windows::KeyHelper::ENV_KEY do
-    values 'Path' => path.join(';')
+action :add do
+  env "PATH" do
+  	action :create
+  	delim ";"
+  	value new_resource.path
   end
-  timeoutVars
+  
 end
 
 action :remove do
-  path = ENV['PATH'].split(';')
-  path.delete(new_resource.path)
-  windows_registry Windows::KeyHelper::ENV_KEY do
-    values 'Path' => path.join(';')
+  env "PATH" do
+  	action :remove
+  	delim ";"
+  	value new_resource.path
   end
-  timeoutVars
-end
-
-private 
-# Notify all processes that the environmental variables have been updated.
-def timeoutVars
-  # Dosent seem to work... You may need to re-login
-  Chef::Log.debug("Telling other processes that the env vars have been updated")
-  require 'Win32API'
-  sendMessageTimeout = Win32API.new('user32', 'SendMessageTimeout', 'LLLPLLP', 'L') 
-  hwnd_broadcast = 0xffff
-  wm_settingchange = 0x001A
-  smto_abortifhung = 2
-  result = 0
-  sendMessageTimeout.call(hwnd_broadcast, wm_settingchange, 0, 'Environment', smto_abortifhung, 5000, result)
 end
