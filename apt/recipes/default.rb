00539db9e176cb4232febe51e73f4ba243a67347
jtimberman <joshua@opscode.com>
2011-08-26 21:04:32 -0600

    COOK-136, correct copyright and kill whitespace

--- apt/recipes/default.rb
+++ apt/recipes/default.rb
@@ -2,15 +2,15 @@
 # Cookbook Name:: apt
 # Recipe:: default
 #
-# Copyright 2008, OpsCode, Inc.
+# Copyright 2008-2011, Opscode, Inc.
 # Copyright 2009, Bryan McLellan <btm@loftninjas.org>
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #     http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -32,8 +32,8 @@ end
 execute "apt-get-update-periodic" do
   command "apt-get update"
   only_if do
-    File.exists?('/var/lib/apt/periodic/update-success-stamp') && 
-    File.mtime('/var/lib/apt/periodic/update-success-stamp') < Time.now - 86400 
+    File.exists?('/var/lib/apt/periodic/update-success-stamp') &&
+    File.mtime('/var/lib/apt/periodic/update-success-stamp') < Time.now - 86400
   end
 end
 
