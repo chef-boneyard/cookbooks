2b0f1de21d149f83fa506c2d7b37bf5e178e16d1
Seth Chisamore <schisamo@opscode.com>
2011-08-30 12:35:45 -0400

    reworked Windows::Helper into a module and added new methods

--- windows/libraries/helper.rb
+++ windows/libraries/helper.rb
@@ -19,11 +19,44 @@
 #
 
 module Windows
-  class Helper
+  module Helper
 
-    def self.win_friendly_path(path)
+    # returns windows friendly version of the provided path, 
+    # ensures backslashes are used everywhere
+    def win_friendly_path(path)
       path.gsub(::File::SEPARATOR, ::File::ALT_SEPARATOR) if path
+    end
+
+    # account for Window's wacky File System Redirector
+    # http://msdn.microsoft.com/en-us/library/aa384187(v=vs.85).aspx
+    # especially important for 32-bit processes (like Ruby) on a 
+    # 64-bit instance of Windows.
+    def locate_sysnative_cmd(cmd)
+      if ::File.exists?("#{ENV['WINDIR']}\\sysnative\\#{cmd}")
+        "#{ENV['WINDIR']}\\sysnative\\#{cmd}"
+      elsif ::File.exists?("#{ENV['WINDIR']}\\system32\\#{cmd}")
+        "#{ENV['WINDIR']}\\system32\\#{cmd}"
+      else
+        cmd
+      end
+    end
+
+    # Create a feature provider dependent value object.
+    # mainly created becasue Windows Feature names are 
+    # different based on whether dism.exe or servicemanagercmd.exe 
+    # is used for installation
+    def value_for_feature_provider(provider_hash)
+      p = Chef::Platform.find_provider_for_node(node, :windows_feature)
+      key = p.to_s.downcase.split('::').last
+      provider_hash[key] || provider_hash[key.to_sym]
+    end
+
+    # singleton instance of the Windows Version checker
+    def win_version
+      @win_version ||= Windows::Version.new
     end
 
   end
 end
+
+Chef::Recipe.send(:include, Windows::Helper)
