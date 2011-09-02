5abdbbe1dd5e4f8165adae6aaf0fb9449cf4b662
Seth Chisamore <schisamo@opscode.com>
2011-09-02 08:50:15 -0400

    refactor for mac os x platform support
    * split default recipe into source and package recipe (follows pattern of other cookbooks)
    * externalize source installation metadata into attributes
    * verify mac os x platform support

--- gecode/recipes/default.rb
+++ gecode/recipes/default.rb
@@ -1,7 +1,4 @@
 #
-# Author:: Christopher Walters (<cw@opscode.com>)
-# Author:: Nuo Yan (<nuo@opscode.com>)
-# Author:: Joshua Timberman (<joshua@opscode.com>)
 # Author:: Seth Chisamore (<schisamo@opscode.com>)
 # Cookbook Name:: gecode
 # Recipe:: default
@@ -20,46 +17,4 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-case node['platform']
-when 'ubuntu','debian'
-
-  include_recipe 'apt'
-
-  # use opscode apt repo for older releases
-  if (platform?("debian") && (node.platform_version.to_f < 7.0)) || 
-      (platform?("ubuntu") && (node.platform_version.to_f < 11.0))
-
-    # add Opscode's apt repo to sources
-    apt_repository "opscode" do
-      uri "http://apt.opscode.com"
-      components ["main"]
-      distribution node['lsb']['codename']
-      key "2940ABA983EF826A"
-      keyserver "pgpkeys.mit.edu"
-      action :add
-    end
-
-  end
-
-  apt_package 'libgecode-dev' do
-    action :install
-  end
-
-when 'redhat','centos'
-
-  include_recipe 'build-essential'
-
-  bash "build gecode from source" do
-    cwd "/tmp"
-    code <<-EOH
-    curl -C - -O  http://www.gecode.org/download/gecode-3.5.0.tar.gz
-    tar zxvf gecode-3.5.0.tar.gz
-    cd gecode-3.5.0 && ./configure
-    make && make install
-    EOH
-    not_if {::File.exists? "/usr/local/lib/libgecodekernel.so" }
-  end
-
-else
-  raise "This recipe does not yet support installing Gecode 3.5.0+ for your platform"
-end
+include_recipe "gecode::#{node['gecode']['install_method']}"
