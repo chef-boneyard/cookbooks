f169dbe79a1a4204dab6887e42399958c068d8ba
Patrick Connolly <patrick.c.connolly@gmail.com>
2011-09-11 15:00:43 -0400

    [COOK-747] Added 'pear upgrade' to php cookbook so that PEAR installer is brought to most recent version.

--- php/recipes/package.rb
+++ php/recipes/package.rb
@@ -34,6 +34,11 @@ pkgs.each do |pkg|
   end
 end
 
+bash "Upgrading PEAR installer" do
+  user "root"
+  code "pear upgrade"
+end
+
 template "#{node['php']['conf_dir']}/php.ini" do
   source "php.ini.erb"
   owner "root"
