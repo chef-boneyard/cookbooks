1f9fc27fb835a9ee6d5f417d6a267041b7419e51
Tim Smith <tim.smith@webtrends.com>
2011-09-14 11:36:46 -0700

    Edited iis/metadata.rb via GitHub

--- iis/metadata.rb
+++ iis/metadata.rb
@@ -1,7 +1,7 @@
 maintainer       "Opscode, Inc."
 maintainer_email "cookbooks@opscode.com"
 license          "Apache 2.0"
-description      "Installs/Configures Microsoft Internet Information Services 7.5"
+description      "Installs/Configures Microsoft Internet Information Services 7.0/7.5"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "1.0.2"
 supports         "windows"
