f46e25bf8d75ca62363a4e5eaf747f062c9eeae8
Michael Wynholds <mike@carbonfive.com>
2011-09-26 10:37:34 -0700

    Update metadata to include upgrade recipe

--- apt/metadata.rb
+++ apt/metadata.rb
@@ -5,6 +5,7 @@ description       "Configures apt and apt services and an LWRP for managing apt 
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version           "1.1.2"
 recipe            "apt", "Runs apt-get update during compile phase and sets up preseed directories"
+recipe            "apt::upgrade", "Runs apt-get upgrade -y"
 recipe            "apt::cacher", "Set up an APT cache"
 recipe            "apt::cacher-client", "Client for the apt::cacher server"
 
