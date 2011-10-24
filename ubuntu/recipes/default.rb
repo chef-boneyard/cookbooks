b7646fa94b65148f9e0768c653f5be0927bd93d6
James Hiscock <bockit@bigpond.net.au>
2011-10-25 11:00:48 +1200

    The apt cookbook defines the 'apt-get-update' for the actual apt-get update command. Made this recipe use that rather than a broken 'apt-get update'.

--- ubuntu/recipes/default.rb
+++ ubuntu/recipes/default.rb
@@ -22,6 +22,6 @@ include_recipe "apt"
 template "/etc/apt/sources.list" do
   mode 0644
   variables :code_name => node[:lsb][:codename]
-  notifies :run, resources(:execute => "apt-get update"), :immediately
+  notifies :run, resources(:execute => "apt-get-update"), :immediately
   source "sources.list.erb"
 end
