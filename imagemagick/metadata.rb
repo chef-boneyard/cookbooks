bd0c7b510cece392b93a5be3631c9d593f279c15
jtimberman <joshua@opscode.com>
2011-11-07 12:11:07 -0700

    imagemagick v0.2.2

--- imagemagick/metadata.rb
+++ imagemagick/metadata.rb
@@ -2,8 +2,8 @@ maintainer       "Opscode, Inc."
 maintainer_email "cookbooks@opscode.com"
 license          "Apache 2.0"
 description      "Installs/Configures imagemagick"
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-version          "0.2.0"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.2.2"
 
 recipe "imagemagick", "Installs imagemagick package"
 recipe "imagemagick::devel", "Installs imagemagick development libraries"
