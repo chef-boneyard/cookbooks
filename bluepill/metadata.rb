50d18be3ef5706e8251cdb35ee073f3dd0c6c85a
jtimberman <joshua@opscode.com>
2011-11-07 15:22:21 -0700

    bluepill v0.2.2

--- bluepill/metadata.rb
+++ bluepill/metadata.rb
@@ -3,5 +3,5 @@ maintainer_email "cookbooks@opscode.com"
 license          "Apache 2.0"
 description      "Installs bluepill gem and configures to manage services, includes bluepill_service LWRP"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.2.0"
+version          "0.2.2"
 recipe "bluepill::default", "Installs bluepill rubygem and set up management directories"
