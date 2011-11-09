32bbf3418c7e8f6067036d50b14d02091d81da6d
jtimberman <joshua@opscode.com>
2011-11-09 11:36:22 -0700

    COOK-598, make sure to use Array, cant .each a string

--- vim/recipes/default.rb
+++ vim/recipes/default.rb
@@ -21,9 +21,9 @@
 # * vim-minimal gives you /bin/vi
 # * vim-enhanced gives you /usr/bin/vim
 vim_base_pkgs = value_for_platform(
-  ["ubuntu", "debian", "arch"] => { "default" => "vim" },
+  ["ubuntu", "debian", "arch"] => { "default" => ["vim"] },
   ["redhat", "centos", "fedora", "scientific"] => { "default" => ["vim-minimal","vim-enhanced"] },
-  "default" => "vim"
+  "default" => ["vim"]
 )
 
 vim_base_pkgs.each do |vim_base_pkg|
