ad2dba1af491ab173a993f9b586b5675016a7ac9
jtimberman <joshua@opscode.com>
2011-11-09 10:48:11 -0700

    pxe_dust v1.1.2

--- pxe_dust/metadata.rb
+++ pxe_dust/metadata.rb
@@ -3,11 +3,10 @@ maintainer_email "cookbooks@opscode.com"
 license          "Apache 2.0"
 description      "Configures installing operating systems with preseed and kickstart files via PXE booting."
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "1.3"
+version          "1.1.2"
 depends          "apache2"
 depends          "tftp"
 
-#tested with Ubuntu, assume Debian works similarly 
-%w{ ubuntu }.each do |os|
+%w{ ubuntu debian }.each do |os|
   supports os
 end
