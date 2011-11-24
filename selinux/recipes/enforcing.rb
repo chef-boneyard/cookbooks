56cd47d67a6b5012018fb4fb63157d35b204daff
Bryan Berry <bryan.berry@gmail.com>
2011-11-24 11:04:51 +0100

    fix syntax error, change order of resources in selinux ckbk

--- selinux/recipes/enforcing.rb
+++ selinux/recipes/enforcing.rb
@@ -18,16 +18,17 @@
 # limitations under the License.
 #
 
-execute "enable selinux enforcement" do
-  not_if "getenforce | grep -qx 'Enforcing'"
-  command "setenforce 1"
-  action :run
-end
 
 template "/etc/selinux/config" do
   source "sysconfig/selinux.erb"
   variables(
-    :selinux => "enforcing",
-    :selinuxtype => "targeted",
+	:selinux => "enforcing",
+	:selinuxtype => "targeted"
   )
+end
+
+execute "enable selinux enforcement" do
+  not_if "getenforce | grep -qx 'Enforcing'"
+  command "setenforce 1"
+  action :run
 end
