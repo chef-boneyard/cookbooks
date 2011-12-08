5ee0e11c1710b144b6ee78fc44a061d4e2ffb5ab
jtimberman <joshua@opscode.com>
2011-12-08 12:31:07 -0700

    [COOK-853] - Add other EL platforms to case for epel

--- git/recipes/default.rb
+++ git/recipes/default.rb
@@ -19,7 +19,7 @@
 case node[:platform]
 when "debian", "ubuntu"
   package "git-core"
-when "centos"
+when "centos","redhat","scientific","fedora"
   case node[:platform_version].to_i
   when 5
     include_recipe "yum::epel"
