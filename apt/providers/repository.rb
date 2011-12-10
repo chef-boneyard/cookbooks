e735cbe7a7347c958c247556d33bf07892730a12
Sebastian Boehm <sebastian@sometimesfood.org>
2011-12-10 11:17:08 +0100

    Clean up apt key installation and repo file building

--- apt/providers/repository.rb
+++ apt/providers/repository.rb
@@ -17,33 +17,60 @@
 # limitations under the License.
 #
 
+# install apt key from keyserver
+def install_key_from_keyserver(key, keyserver)
+  unless system("apt-key list | grep #{key}")
+    execute "install-key #{key}" do
+      command "apt-key adv --keyserver #{keyserver} --recv #{key}"
+      action :nothing
+    end.run_action(:run)
+    new_resource.updated_by_last_action(true)
+  end
+end
+
+# install apt key from URI
+def install_key_from_uri(uri)
+  key_name = uri.split(/\//).last
+  cached_keyfile = "#{Chef::Config[:file_cache_path]}/#{key_name}"
+  unless ::File.exists?(cached_keyfile)
+    remote_file cached_keyfile do
+      source uri
+      mode "0644"
+      action :nothing
+    end.run_action(:run)
+
+    execute "install-key #{key_name}" do
+      command "apt-key add #{cached_keyfile}"
+      action :nothing
+    end.run_action(:run)
+    new_resource.updated_by_last_action(true)
+  end
+end
+
+# build repo file contents
+def build_repo(uri, distribution, components, add_deb_src)
+  components = components.join(' ') if components.respond_to?(:join)
+  repo_info = "#{uri} #{distribution} #{components}\n"
+  repo =  "deb     #{repo_info}"
+  repo << "deb-src #{repo_info}" if add_deb_src
+  repo
+end
+
 action :add do
     new_resource.updated_by_last_action(false)
 
     # add key
     if new_resource.keyserver && new_resource.key
-      execute "install-key #{new_resource.key}" do
-        command "apt-key adv --keyserver #{new_resource.keyserver} --recv #{new_resource.key}"
-        action :nothing
-        not_if "apt-key list | grep #{new_resource.key}"
-      end.run_action(:run)
+      install_key_from_keyserver(new_resource.key, new_resource.keyserver)
     elsif new_resource.key && (new_resource.key =~ /http/)
-      key_name = new_resource.key.split(/\//).last
-      remote_file "#{Chef::Config[:file_cache_path]}/#{key_name}" do
-        source new_resource.key
-        mode "0644"
-        action :nothing
-      end.run_action(:create_if_missing)
-      execute "install-key #{key_name}" do
-        command "apt-key add #{Chef::Config[:file_cache_path]}/#{key_name}"
-        action :nothing
-      end.run_action(:run)
+      install_key_from_uri(new_resource.key)
     end
 
-    # build our listing
-    repo_info = "#{new_resource.uri} #{new_resource.distribution} #{new_resource.components.join(" ")}"
-    repository = "deb #{repo_info}\n"
-    repository += "deb-src #{repo_info}\n" if new_resource.deb_src
+    # build repo file
+    repository = build_repo(new_resource.uri,
+                            new_resource.distribution,
+                            new_resource.components,
+                            new_resource.deb_src)
 
     repo_file = file "/etc/apt/sources.list.d/#{new_resource.repo_name}-source.list" do
       owner "root"
