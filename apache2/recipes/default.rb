2537eefc6992bb680a5a9c7c79f826c95232b3f6
jtimberman <joshua@opscode.com>
2011-12-13 23:58:56 -0700

    [COOK-548] - check if chef knows about perl before installing

--- apache2/recipes/default.rb
+++ apache2/recipes/default.rb
@@ -61,7 +61,7 @@ if platform?("redhat", "centos", "scientific", "fedora", "arch", "suse", "freebs
     action :create
   end
 
-  package "perl"
+  package "perl" unless node['languages']['perl'].attribute?('version')
 
   cookbook_file "/usr/local/bin/apache2_module_conf_generate.pl" do
     source "apache2_module_conf_generate.pl"
