ea5a101cc529b6300a3e495fd1697a28a4807894
Jamie Winsor <jamie@vialstudios.com>
2012-01-14 17:34:53 -0800

    Add reload support to NGINX service
    all configuration changes will notify a reload to the NGINX service

--- nginx/definitions/nginx_site.rb
+++ nginx/definitions/nginx_site.rb
@@ -22,13 +22,13 @@ define :nginx_site, :enable => true do
   if params[:enable]
     execute "nxensite #{params[:name]}" do
       command "/usr/sbin/nxensite #{params[:name]}"
-      notifies :restart, resources(:service => "nginx")
+      notifies :reload, resources(:service => "nginx")
       not_if do ::File.symlink?("#{node[:nginx][:dir]}/sites-enabled/#{params[:name]}") end
     end
   else
     execute "nxdissite #{params[:name]}" do
       command "/usr/sbin/nxdissite #{params[:name]}"
-      notifies :restart, resources(:service => "nginx")
+      notifies :reload, resources(:service => "nginx")
       only_if do ::File.symlink?("#{node[:nginx][:dir]}/sites-enabled/#{params[:name]}") end
     end
   end
