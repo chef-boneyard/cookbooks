3bf6f424f2c8aaacd5f0d46927ba6b017c1e0ffd
Jake Vanderdray <jvanderdray@customink.com>
2012-01-24 15:30:05 -0500

    Updating nrpe LWRP so that it no longer uses a template to write out the file, it now can handle checks that don't have warning or critical flags and also fixing the notification sytax for restarting nrpe process.

--- nagios/resources/nrpecheck.rb
+++ nagios/resources/nrpecheck.rb
@@ -22,8 +22,8 @@ actions :add, :remove
 # Name of the nrpe check, used for the filename and the command name
 attribute :command_name, :kind_of => String, :name_attribute => true
 
-attribute :warning_condition, :kind_of => String
-attribute :critical_condition, :kind_of => String
+attribute :warning_condition, :kind_of => String, :default => nil
+attribute :critical_condition, :kind_of => String, :default => nil
 attribute :command, :kind_of => String
 attribute :parameters, :kind_of => String, :default => nil
 
