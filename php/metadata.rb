e5876aaa632c3846e00ae41cb049c0bfa7beb956
jtimberman <joshua@opscode.com>
2012-02-13 18:38:32 -0700

    php v1.0.2

--- php/metadata.rb
+++ php/metadata.rb
@@ -2,7 +2,7 @@ maintainer        "Opscode, Inc."
 maintainer_email  "cookbooks@opscode.com"
 license           "Apache 2.0"
 description       "Installs and maintains php and php modules"
-version           "1.0.0"
+version           "1.0.2"
 
 depends "build-essential"
 depends "xml"
