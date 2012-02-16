29611c715b798eb695689b3a60a6516a78f49a6d
Marcos Tapajos <tapajos@gmail.com>
2012-02-16 09:26:27 -0200

    cookbook for installing/configuring ruby-1.9.3p0 on ubuntu

---
+++ ruby-1.9.3/metadata.rb
@@ -0,0 +1,8 @@
+maintainer        "Marcos Tapajós"
+maintainer_email  "marcos@tapajos.me"
+license           "Apache 2.0"
+description       "Installs Ruby 1.9.3"
+version           "1.0.0"
+recipe            "ruby-1.9.3", "Installs Ruby 1.9.3 on Ubuntu Lucid"
+
+supports 'ubuntu'
