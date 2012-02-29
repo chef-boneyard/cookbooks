0958a9ed699bae4b1ab4a73cb217bb401797e95a
Yann ROBIN <me.show@gmail.com>
2012-02-29 11:19:50 +0100

    add attributes bindings

--- iis/resources/site.rb
+++ iis/resources/site.rb
@@ -26,5 +26,6 @@ attribute :port, :kind_of => Integer
 attribute :path, :kind_of => String
 attribute :protocol, :kind_of => Symbol, :default => :http, :equal_to => [:http, :https]
 attribute :host_header, :kind_of => String, :default => nil
+attribute :bindings, :kind_of => String, :default => nil
 
 attr_accessor :exists, :running
