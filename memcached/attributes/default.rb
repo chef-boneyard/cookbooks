6f7e2ea80057df1ed8be4a24cb85f572d7817ae8
Javier Frias <jfrias@gmail.com>
2012-03-01 19:39:47 -0500

    Added support for centos/rhel systems which use /etc/sysconfig/memcached

--- memcached/attributes/default.rb
+++ memcached/attributes/default.rb
@@ -2,3 +2,4 @@ default[:memcached][:memory] = 64
 default[:memcached][:port] = 11211
 default[:memcached][:user] = "nobody"
 default[:memcached][:listen] = "0.0.0.0"
+default[:memcached][:maxconn] = "1024"
