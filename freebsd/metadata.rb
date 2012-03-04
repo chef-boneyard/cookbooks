a301359fafb5ad2afaaceb07c492258de349fb09
Andrea Campi <andrea.campi@zephirworks.com>
2012-03-04 16:03:00 +0100

    [COOK-1074] Add a FreeBSD-specific cookbook.

---
+++ freebsd/metadata.rb
@@ -0,0 +1,6 @@
+maintainer       "ZephirWorks"
+maintainer_email "andrea.campi@zephirworks.com"
+license          "Apache 2.0"
+description      "Installs/Configures freebsd"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.0.1"
