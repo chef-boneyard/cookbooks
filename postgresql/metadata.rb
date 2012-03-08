ee2b20af6adafcbbce039dd8eeee1867023d3243
jtimberman <joshua@opscode.com>
2012-03-07 20:58:45 -0700

    postgresql v0.99.4

--- postgresql/metadata.rb
+++ postgresql/metadata.rb
@@ -3,7 +3,7 @@ maintainer_email  "cookbooks@opscode.com"
 license           "Apache 2.0"
 description       "Installs and configures postgresql for clients or servers"
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           "0.99.2"
+version           "0.99.4"
 recipe            "postgresql", "Includes postgresql::client"
 recipe            "postgresql::client", "Installs postgresql client package(s)"
 recipe            "postgresql::server", "Installs postgresql server packages, templates"
