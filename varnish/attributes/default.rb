f39ae1c657ad2814b60bb88150acae1994b1bd47
Mark Sonnabaum <mark@sonnabaum.com>
2012-03-11 12:19:00 -0500

    [COOK-873] - Added basic configuration via attributes for varnish.

--- varnish/attributes/default.rb
+++ varnish/attributes/default.rb
@@ -3,3 +3,8 @@ when "debian","ubuntu"
   set[:varnish][:dir]     = "/etc/varnish"
   set[:varnish][:default] = "/etc/default/varnish"
 end
+
+default[:varnish][:listen_port] = 6081
+default[:varnish][:admin_port] = 6082
+default[:varnish][:backend_host] = "localhost"
+default[:varnish][:backend_port] = 8080
