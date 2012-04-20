18ce9f86d4744ee8e7611669941e63df6d9f304a
Klaus Meyer <spam@klaus-meyer.net>
2012-04-20 16:56:14 +0200

    fixed postgres::server for debian 6.0.4

--- postgresql/recipes/server_debian.rb
+++ postgresql/recipes/server_debian.rb
@@ -41,9 +41,9 @@ service "postgresql" do
     end
   when "debian"
     case
-    when platform_version.to_f <= 5.0
+    when node['platform_version'].to_f <= 5.0
       service_name "postgresql-#{node['postgresql']['version']}"
-    when platform_version =~ /squeeze/
+    when node['platform_version'] =~ /squeeze/
       service_name "postgresql"
     else
       service_name "postgresql"
