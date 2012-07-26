7113727194c37f69cd23361cd98857ea935f39bc
William Herry <WilliamHerryChina@Gmail.com>
2012-07-26 17:49:04 +0800

    add arch to epel, most of our server are 64bit

--- yum/recipes/epel.rb
+++ yum/recipes/epel.rb
@@ -19,12 +19,13 @@
 
 major = node['platform_version'].to_i
 epel  = node['yum']['epel_release']
+arch  = node['kernel']['machine']
 
 # If rpm installation from a URL supported 302's, we'd just use that.
 # Instead, we get to remote_file then rpm_package.
 
 remote_file "#{Chef::Config[:file_cache_path]}/epel-release-#{epel}.noarch.rpm" do
-  source "http://download.fedoraproject.org/pub/epel/#{major}/i386/epel-release-#{epel}.noarch.rpm"
+  source "http://download.fedoraproject.org/pub/epel/#{major}/#{arch}/epel-release-#{epel}.noarch.rpm"
   not_if "rpm -qa | egrep -qx 'epel-release-#{epel}(|.noarch)'"
   notifies :install, "rpm_package[epel-release]", :immediately
 end
